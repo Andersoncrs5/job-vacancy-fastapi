e4bf7e277b2144339ad16ad2998a99bb03dfbbd9
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-17 16:31:14 -0300

    set base to user_service

---
+++ app/services/base/user_service_base.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from app.schemas.user_schemas import CreateUserDTO, UpdateUserDTO
+from app.configs.db.database import UserEntity
+
+class UserServiceBase(ABC):
+    @abstractmethod
+    async def exists_by_email(self, email: str) -> bool:
+        pass
+
+    @abstractmethod
+    async def set_refresh_token(self, refresh_token: str, user: UserEntity) -> UserEntity:
+        pass
+    
+    @abstractmethod
+    async def update(self, user: UserEntity, dto: UpdateUserDTO) -> UserEntity:
+        pass
+
+    @abstractmethod
+    async def create(self, dto: CreateUserDTO) -> UserEntity:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> (UserEntity | None):
+        pass
+
+    @abstractmethod
+    async def get_by_email(self, email: str) -> (UserEntity | None):
+        pass
+
+    @abstractmethod
+    async def delete(self, user: UserEntity):
+        pass
