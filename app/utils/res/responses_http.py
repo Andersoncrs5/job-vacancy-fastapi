1b16882fdb852aecb1df8dfc9fade5c59920066a
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-17 19:57:46 -0300

    set responses http

---
+++ app/utils/res/responses_http.py
@@ -0,0 +1,22 @@
+from app.utils.res.response_body import ResponseBody
+from typing import Final, Dict, Any
+
+RESPONSE_401: Final[Dict] = {
+    "description": "Token invalid",
+    "model": ResponseBody[None]
+}
+
+RESPONSE_404_USER: Final[Dict] = {
+    "description": "User not found",
+    "model": ResponseBody[None]
+}
+
+RESPONSE_404_POST: Final[Dict] = {
+    "description": "Post not found",
+    "model": ResponseBody[None]
+}
+
+RESPONSE_500: Final[Dict] = {
+    "description": "User not found",
+    "model": ResponseBody[Any]
+}
