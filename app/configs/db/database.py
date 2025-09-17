cf48186f683ef9c2728836d2b84b250c8956b721
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-17 13:41:34 -0300

    set config database

---
+++ app/configs/db/database.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs, AsyncEngine, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing import Final
+from sqlalchemy import DateTime, String, func
+from datetime import datetime
+
+load_dotenv()
+
+DATABASE_URL: Final[str | None] = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is None")
+
+engine: Final[AsyncEngine] = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal: Final[async_sessionmaker[AsyncSession]] = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+class UserEntity(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True, index=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    refresh_token: Mapped[str | None] = mapped_column(String(), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
