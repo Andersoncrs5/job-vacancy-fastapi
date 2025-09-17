cdea58049b39692c1715f800f8cc3834a3b68937
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-17 13:41:45 -0300

    set repository base to userEntity

---
+++ app/repositories/base/user_repository_base.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from app.configs.db.database import UserEntity
+
+class UserRepositoryBase(ABC):
+
+    @abstractmethod
+    async def add(self, user: UserEntity) -> UserEntity:
+        pass
+
+    @abstractmethod
+    async def save(self, user: UserEntity) -> UserEntity:
+        pass
+
+    @abstractmethod
+    async def delete(self, user: UserEntity) -> None:
+        pass
+
+    @abstractmethod
+    async def exists_by_email(self, email: str) -> bool:
+        pass
+
+    @abstractmethod
+    async def get_by_email(self, email: str) -> (UserEntity | None):
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> (UserEntity | None):
+        pass
+
