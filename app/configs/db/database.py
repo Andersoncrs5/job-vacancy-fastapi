09347026f23ed05af71b25075cde711f30d9fede
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-18 18:31:27 -0300

    set table user PostUserEntity

--- app/configs/db/database.py
+++ app/configs/db/database.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs, AsyncEngine, AsyncSession
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from typing import Final
-from sqlalchemy import DateTime, String, func
+from sqlalchemy import DateTime, String, func, Text, ForeignKey, Boolean
 from datetime import datetime
 
 load_dotenv()
@@ -31,9 +31,13 @@ class UserEntity(Base):
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True, index=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    avatar_url: Mapped[str | None] = mapped_column(String(255), nullable=True)
     refresh_token: Mapped[str | None] = mapped_column(String(), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    posts: Mapped[list["PostUserEntity"]] = relationship("PostUserEntity", back_populates="owner")
 
     def to_user_out(self):
         from app.schemas.user_schemas import UserOUT
@@ -43,3 +47,18 @@ class UserEntity(Base):
             name = self.name,
             email = self.email,
         )
+
+class PostUserEntity(Base):
+    __tablename__ = "posts_user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    url_image: Mapped[str] = mapped_column(Text, nullable=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    owner: Mapped["UserEntity"] = relationship("UserEntity", back_populates="posts")
