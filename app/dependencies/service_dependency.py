9026714ef2e7324d78474271307739f30ae67da6
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-19 15:57:17 -0300

    new dependency to category_service_provider

--- app/dependencies/service_dependency.py
+++ app/dependencies/service_dependency.py
@@ -3,9 +3,15 @@ from fastapi import Depends
 from app.configs.db.database import get_db
 from app.repositories.providers.user_repository_provider import UserRepositoryProvider
 from app.services.providers.user_service_provider import UserServiceProvider
+from app.repositories.providers.category_repository_provider import CategoryRepositoryProvider
+from app.services.providers.category_service_provider import CategoryServiceProvider
 from app.services.providers.jwt_service_provider import JwtServiceProvider
 from app.services.base.jwt_service_base import JwtServiceBase
 from typing import Final
+
+def get_category_service_provider_dependency(db: AsyncSession = Depends(get_db)) -> CategoryServiceProvider:
+    repository: Final = CategoryRepositoryProvider(db)
+    return CategoryServiceProvider(repository)
 
 def get_user_service_provider_dependency(db: AsyncSession = Depends(get_db)) -> UserServiceProvider:
     repository: Final = UserRepositoryProvider(db)
