1929ab97764392760f386f0e5b8051f7e67fb0a6
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-19 19:47:15 -0300

    set endpoints

--- app/controllers/post_user_controller.py
+++ app/controllers/post_user_controller.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
-from app.configs.db.database import PostUserEntity, UserEntity
+from app.configs.db.database import PostUserEntity, UserEntity, CategoryEntity
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from typing import Final
 from app.utils.res.response_body import ResponseBody
@@ -24,3 +24,437 @@ router: Final[APIRouter] = APIRouter(
 
 bearer_scheme: Final[HTTPBearer] = HTTPBearer()
 
+@router.delete(
+    "/{post_user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBody[None],
+    responses={
+        404: RESPONSE_404_POST_USER,
+    }
+)
+async def delete(
+    post_user_id: int,
+    post_user_service: PostUserServiceProvider = Depends(get_post_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if post_user_id is None or post_user_id <= 0:
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_400_BAD_REQUEST,
+                    message="Post user id is required",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int | None] = jwt_service.extract_user_id(token)
+        if user_id is None or user_id <= 0:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_401_UNAUTHORIZED,
+                    message="You are not authorized",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        post_user: Final[PostUserEntity | None] = await post_user_service.get_by_id(post_user_id)
+        if post_user is None:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Post user not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )  
+        
+        await post_user_service.delete(post_user)
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[None](
+                message="Post deleted with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=None,
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return JSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.get(
+    "/{post_user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBody[PostUserOUT],
+    responses={
+        404: RESPONSE_404_POST_USER,
+    }
+)
+async def get(
+    post_user_id: int,
+    post_user_service: PostUserServiceProvider = Depends(get_post_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if post_user_id is None or post_user_id <= 0:
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_400_BAD_REQUEST,
+                    message="Post user id is required",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int | None] = jwt_service.extract_user_id(token)
+        if user_id is None or user_id <= 0:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_401_UNAUTHORIZED,
+                    message="You are not authorized",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        post_user: Final[PostUserEntity | None] = await post_user_service.get_by_id(post_user_id)
+        if post_user is None:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Post user not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )  
+        
+        post_user_out: Final[PostUserOUT] = post_user.to_out()
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[dict](
+                message="Post found with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=dict(post_user_out),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return JSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.put(
+    '/{post_user_id}',
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBody[PostUserOUT],
+    responses={
+        404: RESPONSE_404_POST_USER,
+        404: RESPONSE_404_USER,
+        404: RESPONSE_404_CATEGORY,
+    }
+)
+async def update(
+    post_user_id: int,
+    dto: UpdatePostUserDTO,
+    post_user_service: PostUserServiceProvider = Depends(get_post_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if post_user_id is None or post_user_id <= 0:
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_400_BAD_REQUEST,
+                    message="Post user id is required",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int | None] = jwt_service.extract_user_id(token)
+        if user_id is None or user_id <= 0:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_401_UNAUTHORIZED,
+                    message="You are not authorized",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        post_user: Final[PostUserEntity | None] = await post_user_service.get_by_id(post_user_id)
+        if post_user is None:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Post user not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )  
+        
+        post_user_updated: Final[PostUserEntity] = await post_user_service.update(post_user, dto)
+
+        post_user_out: Final[PostUserOUT] = post_user_updated.to_out()
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[dict](
+                message="Post updated with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=dict(post_user_out),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return JSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.get(
+    '',
+    status_code=status.HTTP_200_OK,
+    response_model=Page[PostUserOUT],
+    )
+async def get_all(
+    filter: PostUserFilter = Depends(),
+    post_user_service: PostUserServiceProvider = Depends(get_post_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int | None] = jwt_service.extract_user_id(token)
+        if user_id is None or user_id <= 0:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_401_UNAUTHORIZED,
+                    message="You are not authorized",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        categories: Final[list[PostUserEntity]] = await post_user_service.get_all_filter(filter)
+
+        return paginate(categories)
+
+    except Exception as e:
+        return JSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.post(
+    '/{category_id}',
+    response_model=ResponseBody[PostUserOUT],
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        404: RESPONSE_404_USER,
+        404: RESPONSE_404_CATEGORY,
+    }
+)
+async def create(
+    category_id: int,
+    dto: CreatePostUserDTO,
+    post_user_service: PostUserServiceProvider = Depends(get_post_user_service_provider_dependency),
+    user_service: UserServiceProvider = Depends(get_user_service_provider_dependency),
+    category_service: CategoryServiceProvider = Depends(get_category_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if category_id is None or category_id <= 0:
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_400_BAD_REQUEST,
+                    message="Id is required",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int | None] = jwt_service.extract_user_id(token)
+        if user_id is None or user_id <= 0:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_401_UNAUTHORIZED,
+                    message="You are not authorized",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        user: Final[UserEntity | None] = await user_service.get_by_id(user_id)
+        if user is None:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="User not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )            
+
+        category: Final[CategoryEntity | None] = await category_service.get_by_id(category_id)
+        if category is None:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Category not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )  
+        
+        post_user_created: Final[PostUserEntity] = await post_user_service.create(user, category, dto)
+
+        post_user_out = post_user_created.to_out()
+
+        return JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content=dict(ResponseBody[dict](
+                message="Post created",
+                code=status.HTTP_201_CREATED,
+                status=True,
+                body=dict(post_user_out),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return JSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+add_pagination(router)
