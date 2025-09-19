ef3b019d2faa7bf2c6b9a1dae76c2fe9e231fe59
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-19 18:56:19 -0300

    set dependency to post_user_service_provider

--- app/dependencies/service_dependency.py
+++ app/dependencies/service_dependency.py
@@ -4,10 +4,16 @@ from app.configs.db.database import get_db
 from app.repositories.providers.user_repository_provider import UserRepositoryProvider
 from app.services.providers.user_service_provider import UserServiceProvider
 from app.repositories.providers.category_repository_provider import CategoryRepositoryProvider
+from app.repositories.providers.post_user_repository_provider import PostUserRepositoryProvider
 from app.services.providers.category_service_provider import CategoryServiceProvider
+from app.services.providers.post_user_service_provider import PostUserServiceProvider
 from app.services.providers.jwt_service_provider import JwtServiceProvider
 from app.services.base.jwt_service_base import JwtServiceBase
 from typing import Final
+
+def get_post_user_service_provider_dependency(db: AsyncSession = Depends(get_db)) -> PostUserServiceProvider:
+    repository: Final = PostUserRepositoryProvider(db)
+    return PostUserServiceProvider(repository)
 
 def get_category_service_provider_dependency(db: AsyncSession = Depends(get_db)) -> CategoryServiceProvider:
     repository: Final = CategoryRepositoryProvider(db)
