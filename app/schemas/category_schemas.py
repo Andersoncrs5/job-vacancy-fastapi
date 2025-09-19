aacd59f13946f57f253649e7c758ef8cae74afb3
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-18 22:06:50 -0300

    new method to_category_entity to class CreateCategoryDTO

--- app/schemas/category_schemas.py
+++ app/schemas/category_schemas.py
@@ -7,6 +7,18 @@ class CreateCategoryDTO(BaseModel):
     order: int
     icon_url: str | None
 
+    def to_category_entity(self):
+        from app.configs.db.database import CategoryEntity
+
+        return CategoryEntity(
+            name = self.name,
+            slug = self.slug,
+            description = self.description,
+            order = self.order,
+            icon_url = self.icon_url,
+        )
+
+
 class UpdateCategoryDTO(BaseModel):
     name: str | None
     slug: str | None
