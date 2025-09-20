2a559c3ec8f858b83466b4164cc9a05937b3fafe
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 11:03:29 -0300

    new methods

--- app/services/base/industry_service_base.py
+++ app/services/base/industry_service_base.py
@@ -1,8 +1,13 @@
 from abc import ABC, abstractmethod
-from app.configs.db.database import IndustryEntity
+from app.configs.db.database import IndustryEntity, UserEntity
 from app.schemas.industry_schemas import CreateIndustryDTO, UpdateIndustryDTO
+from app.utils.filter.industry_filter import IndustryFilter
 
 class IndustryServiceBase(ABC):
+
+    @abstractmethod
+    async def get_all_filter(self, filter: IndustryFilter) -> list[IndustryEntity]:
+        pass
 
     @abstractmethod
     async def get_by_id(self, id: int) -> IndustryEntity | None:
@@ -13,6 +18,16 @@ class IndustryServiceBase(ABC):
         pass
 
     @abstractmethod
-    async def create(self, dto: CreateIndustryDTO) -> IndustryEntity:
+    async def create(self, user: UserEntity, dto: CreateIndustryDTO) -> IndustryEntity:
         pass
 
+    @abstractmethod
+    async def update(self, industry: IndustryEntity, dto: UpdateIndustryDTO) -> IndustryEntity:
+        pass
+
+    @abstractmethod
+    async def toggle_is_active(self, industry: IndustryEntity) -> IndustryEntity:
+        pass
+
+
+    
