562f3c581bd37d15500a26e6660441d92dc6c0c3
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 16:42:49 -0300

    added methods

--- app/services/base/enterprise_service_base.py
+++ app/services/base/enterprise_service_base.py
@@ -1,10 +1,31 @@
 from abc import ABC, abstractmethod
-from app.configs.db.database import EnterpriseEntity
+from app.configs.db.database import EnterpriseEntity, UserEntity, IndustryEntity
+from app.schemas.enterprise_schemas import CreateEnterpriseDTO, UpdateEnterpriseDTO
+from app.utils.filter.enterprise_filter import EnterpriseFilter
 
 class EnterpriseServiceBase(ABC):
+
+    @abstractmethod
+    async def get_all_filter(self, filter: EnterpriseFilter) -> list[EnterpriseEntity]:
+        pass
+
+    @abstractmethod
+    async def update(self, enter: EnterpriseEntity, dto: UpdateEnterpriseDTO) -> EnterpriseEntity:
+        pass
+
+    @abstractmethod
+    async def exists_by_name(self, name: str) -> bool:
+        pass
+
+    @abstractmethod
+    async def delete(self, enter: EnterpriseEntity):
+        pass
+
+    @abstractmethod
+    async def create(self, industry: IndustryEntity, user: UserEntity ,dto: CreateEnterpriseDTO) -> EnterpriseEntity:
+        pass
 
     @abstractmethod
     async def get_by_id(self, id: int) -> EnterpriseEntity | None:
         pass
 
-    
