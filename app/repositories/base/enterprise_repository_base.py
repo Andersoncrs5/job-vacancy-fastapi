4d08ffae7e0b5a57a8c93c3b29d1c55dc388a105
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 14:43:49 -0300

    added methods

--- app/repositories/base/enterprise_repository_base.py
+++ app/repositories/base/enterprise_repository_base.py
@@ -1,6 +1,25 @@
 from abc import ABC, abstractmethod
 from app.configs.db.database import EnterpriseEntity
+from app.utils.filter.enterprise_filter import EnterpriseFilter
 
 class EnterpriseRepositoryBase(ABC):
 
+    @abstractmethod
+    async def add(self, enter: EnterpriseEntity) -> EnterpriseEntity:
+        pass
     
+    @abstractmethod
+    async def get_all_filter(self, filter: EnterpriseFilter) -> list[EnterpriseEntity]:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> EnterpriseEntity | None:
+        pass
+
+    @abstractmethod
+    async def save(self, enter: EnterpriseEntity) -> EnterpriseEntity:
+        pass
+
+    @abstractmethod
+    async def delete(self, enter: EnterpriseEntity):
+        pass
--- app/repositories/providers/enterprise_repository_provider.py
+++ app/repositories/providers/enterprise_repository_provider.py
@@ -1,3 +1,42 @@
 from app.repositories.base.enterprise_repository_base import EnterpriseRepositoryBase
+from app.configs.db.database import EnterpriseEntity
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.utils.filter.enterprise_filter import EnterpriseFilter
+from typing import Final
 
 class EnterpriseRepositoryProvider(EnterpriseRepositoryBase):
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_all_filter(self, filter: EnterpriseFilter) -> list[EnterpriseEntity]:
+        stmt = select(EnterpriseEntity)
+
+        stmt = filter.filter(stmt)
+
+        result: Final = await self.db.execute(stmt)
+        all: Final = result.scalars().all()
+        return list(all)
+
+    async def add(self, enter: EnterpriseEntity) -> EnterpriseEntity:
+        self.db.add(enter)
+        await self.db.commit()
+        await self.db.refresh(enter)
+
+        return enter
+
+    async def get_by_id(self, id: int) -> EnterpriseEntity | None:
+        result = await self.db.execute(
+            select(EnterpriseEntity).where(EnterpriseEntity.id == id)
+        )
+        return result.scalars().first()
+
+    async def delete(self, enter: EnterpriseEntity):
+        await self.db.delete(enter)
+        await self.db.commit()
+
+    async def save(self, enter: EnterpriseEntity) -> EnterpriseEntity:
+        await self.db.commit()
+        await self.db.refresh(enter)
+
+        return enter
