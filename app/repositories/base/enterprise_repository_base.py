a479382afcada2f999b2625a610cbd30b957f564
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 18:03:27 -0300

    new method exists_by_user_id

--- app/repositories/base/enterprise_repository_base.py
+++ app/repositories/base/enterprise_repository_base.py
@@ -5,6 +5,10 @@ from app.utils.filter.enterprise_filter import EnterpriseFilter
 class EnterpriseRepositoryBase(ABC):
 
     @abstractmethod
+    async def exists_by_user_id(self, user_id: int) -> bool:
+        pass
+    
+    @abstractmethod
     async def exists_by_name(self, name: str) -> bool:
         pass
     
