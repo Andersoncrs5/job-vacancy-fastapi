19864c909634ecd449c942a09888422f04e16c01
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 09:05:37 -0300

    set industry repository base with methods

---
+++ app/repositories/base/industry_repository_base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from app.configs.db.database import IndustryEntity
+
+class IndustryRepositoryBase(ABC):
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> IndustryEntity | None:
+        pass
+
+    @abstractmethod
+    async def delete(self, industry: IndustryEntity) -> None:
+        pass
+
+    @abstractmethod
+    async def save(self, industry: IndustryEntity) -> IndustryEntity:
+        pass
+
+    @abstractmethod
+    async def add(self, industry: IndustryEntity) -> IndustryEntity:
+        pass
+
+    
