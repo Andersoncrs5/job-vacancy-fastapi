15f818838877f7cb6270558c74288a3c558e806d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 13:26:32 -0300

    new table enterprises e set relationship between user and industry

--- app/configs/db/database.py
+++ app/configs/db/database.py
@@ -41,6 +41,9 @@ class UserEntity(Base):
     categories: Mapped[list["CategoryEntity"]] = relationship("CategoryEntity", back_populates="owner")
     industries: Mapped[list["IndustryEntity"]] = relationship("IndustryEntity", back_populates="owner")
 
+    enterprise: Mapped["EnterpriseEntity"] = relationship("EnterpriseEntity", back_populates="owner", uselist=False)
+
+
     def to_user_out(self):
         from app.schemas.user_schemas import UserOUT
 
@@ -68,6 +71,8 @@ class IndustryEntity(Base):
     
     owner: Mapped["UserEntity"] = relationship("UserEntity", back_populates="industries")
 
+    enterprises: Mapped[list["EnterpriseEntity"]] = relationship("EnterpriseEntity", back_populates="industry")
+
     def to_out(self):
         from app.schemas.industry_schemas import IndustryOUT
 
@@ -83,18 +88,25 @@ class IndustryEntity(Base):
             updated_at = str(self.updated_at),
         )
 
-# class EnterpriseEntity(Base):
-#     __tablename__ = "users"
+class EnterpriseEntity(Base):
+    __tablename__ = "enterprises"
 
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(String(50), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
 
-#     description: Mapped[str | None] = mapped_column(Text, nullable=True)
-#     website_url: Mapped[str | None] = mapped_column(Text, nullable=True)
-#     logo_url: Mapped[str | None] = mapped_column(Text, nullable=True)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+    website_url: Mapped[str | None] = mapped_column(Text, nullable=True)
+    logo_url: Mapped[str | None] = mapped_column(Text, nullable=True)
 
-#     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-#     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+
+    industry_id: Mapped[int] = mapped_column(ForeignKey("industries.id"))
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    owner: Mapped["UserEntity"] = relationship("UserEntity", back_populates="enterprise")
+    industry: Mapped["IndustryEntity"] = relationship("IndustryEntity", back_populates="enterprises")
 
 class CategoryEntity(Base):
     __tablename__ = "categories"
