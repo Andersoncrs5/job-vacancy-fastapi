f1ce5c05c7acf0226e6ed2cbdcabc8ca668bc1fb
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 21:38:23 -0300

    set new column bio in classes

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,23 +1,29 @@
 from pydantic import BaseModel, Field, EmailStr
+from datetime import datetime
 
 class CreateUserDTO(BaseModel):
     name: str = Field(..., min_length=4, max_length=50, description="The name field should be between 4 and 50")
     email: EmailStr = Field(..., min_length=8, max_length=150, description="The email field should be between 8 and 150")
+    bio: str | None = Field(None, max_length=600, description="The bio field should have size max of 50")
     password: str = Field(..., min_length=6, max_length=50, description="The password field should be between 6 and 50")
+    avatar_url: str | None = Field(None, max_length=255, description="The avatar url field should have size max of 255")
 
     def to_user_entity(self):
         from app.configs.db.database import UserEntity
         
         return UserEntity(
-            name= self.name,
-            email= self.email,
-            password= self.password,
+            name = self.name,
+            email = self.email,
+            password = self.password,
+            bio = self.bio,
+            avatar_url = self.avatar_url,
         )
     
 class UpdateUserDTO(BaseModel):
     name: str | None = Field(None, max_length=50, description="The name field should have size max of 50")
     password: str | None = Field(None, max_length=50, description="The password field should have size max of 50")
-    avatar_url: str | None = Field(None, max_length=255, description="The avatar_url field should have size max of 50")
+    bio: str | None = Field(None, max_length=600, description="The bio field should have size max of 600")
+    avatar_url: str | None = Field(None, max_length=255, description="The avatar url field should have size max of 255")
 
 class LoginDTO(BaseModel):
     email: EmailStr = Field(..., min_length=8, max_length=150, description="The email field should be between 8 and 150")
@@ -27,3 +33,9 @@ class UserOUT(BaseModel):
     id: int
     name: str
     email: str
+    avatar_url: str | None
+    bio: str | None
+    created_at: datetime | str
+
+    class Config:
+        from_attributes = True
