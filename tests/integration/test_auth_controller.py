14b0803cbfaae11fa8c74518a476e312b3ebbc0c
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-21 13:12:41 -0300

    set test integration to auth controller

--- tests/integration/test_auth_controller.py
+++ tests/integration/test_auth_controller.py
@@ -9,20 +9,18 @@ import pytest
 client: Final[TestClient] = TestClient(app)
 
 @pytest.mark.asyncio
-def test_register_user():
-    num = random.randint(1000000,10000000000000)
+async def test_register_user():
+    num = random.randint(1000000, 10000000000000)
     dto = CreateUserDTO(
-        name = f"user {num}",
-        email = f"user{num}@example.com",
-        password = str(num),
-        bio = None,
-        avatar_url = None,
+        name=f"user {num}",
+        email=f"user{num}@example.com",
+        password=str(num),
+        bio=None,
+        avatar_url=None,
     )
 
-    response = client.post(
-        "/api/v1/auth/register",
-        json=dict(dto)
-    )
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response = await ac.post("/api/v1/auth/register", json=dict(dto))
 
     assert response.status_code == 201
 
@@ -38,11 +36,11 @@ async def test_register_and_login_user():
     )
 
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        response = await ac.post("/api/v1/auth/register", json=dto.dict())
-        assert response.status_code == 201
+        response = await ac.post("/api/v1/auth/register", json=dict(dto))
+    assert response.status_code == 201
 
 
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         dto_login = LoginDTO(email=dto.email, password=dto.password)
-        response = await ac.post("/api/v1/auth/login", json=dto_login.dict())
-        assert response.status_code == 200
+        response = await ac.post("/api/v1/auth/login", json=dict(dto_login))
+    assert response.status_code == 200
