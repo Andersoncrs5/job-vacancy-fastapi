cadfd36ae3243d4aa6bbc959b86c876a4672ff55
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 21:45:00 -0300

    set methods

--- app/services/base/favorite_post_user_service_base.py
+++ app/services/base/favorite_post_user_service_base.py
@@ -1,3 +1,24 @@
 from abc import ABC, abstractmethod
+from app.configs.db.database import FavoritePostUserEntity, PostUserEntity, UserEntity
 
 class FavoritePostUserServiceBase(ABC):
+
+    @abstractmethod
+    async def delete(self, favo: FavoritePostUserEntity):
+        pass
+    
+    @abstractmethod
+    async def add(self, post: PostUserEntity, user: UserEntity) -> FavoritePostUserEntity:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> FavoritePostUserEntity | None:
+        pass
+
+    @abstractmethod
+    async def exists_by_user_id_post_id(self, user_id: int, post_id: int) -> bool:
+        pass
+
+    @abstractmethod
+    async def get_all_by_user_id_just_post(self, user_id: int) -> list[PostUserEntity]:
+        pass
