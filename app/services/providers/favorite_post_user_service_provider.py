04faf2884226c29636409e997e7db8b6f352fff1
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-20 21:47:14 -0300

    impl method delete

--- app/services/providers/favorite_post_user_service_provider.py
+++ app/services/providers/favorite_post_user_service_provider.py
@@ -1,3 +1,13 @@
 from app.services.base.favorite_post_user_service_base import FavoritePostUserServiceBase
+from app.configs.db.database import FavoritePostUserEntity, PostUserEntity, UserEntity
+from app.repositories.providers.favorite_posts_user_repository_provider import FavoritePostUserRepositoryProvider
 
 class FavoritePostUserServiceProvider(FavoritePostUserServiceBase):
+    def __init__(self, repository: FavoritePostUserRepositoryProvider):
+        self.repository = repository
+
+    async def delete(self, favo: FavoritePostUserEntity):
+        await self.repository.delete(favo)
+
+    
+    
