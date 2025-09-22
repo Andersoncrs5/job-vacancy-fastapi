4c38a634393049139cb92a710a28d629caf427d3
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-21 21:09:36 -0300

    new test upate user

--- tests/integration/test_user_controller.py
+++ tests/integration/test_user_controller.py
@@ -3,6 +3,7 @@ from typing import Final
 from app.schemas.category_schemas import *
 from tests.integration.helper import create_and_login_user, create_category
 from main import app
+from app.schemas.user_schemas import UserOUT, UpdateUserDTO
 from httpx import ASGITransport, AsyncClient
 import pytest
 import random
@@ -12,11 +13,37 @@ client: Final[TestClient] = TestClient(app)
 URL: Final[str] = "/api/v1/user"
 
 @pytest.mark.asyncio
-async def delete():
+async def test_update_user():
+    user_data: Final = await create_and_login_user()
+
+    dto = UpdateUserDTO(
+        name = "user updated",
+        password = None,
+        bio = "bio updated",
+        avatar_url = None
+    )
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response = await ac.put(URL, json=dict(dto), headers={"Authorization": f"Bearer {user_data.tokens.token}"})
+
+    data = response.json()
+
+    assert response.status_code == 200
+
+    assert data['message'] == "User updated with successfully"
+    assert data['code'] == 200
+    assert data['status'] == True
+    assert data['body']['bio'] == dto.bio
+    assert data['body']['name'] == dto.name
+    assert data['version'] == 1
+    assert data['path'] is None
+
+@pytest.mark.asyncio
+async def test_delete_user():
     user_data: Final = await create_and_login_user()
 
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        response = await ac.get(URL+"/me", headers={"Authorization": f"Bearer {user_data.tokens.token}"})
+        response = await ac.delete(URL, headers={"Authorization": f"Bearer {user_data.tokens.token}"})
 
     assert response.status_code == 200
 
@@ -30,11 +57,11 @@ async def delete():
     assert data['path'] is None
 
 @pytest.mark.asyncio
-async def get_user():
+async def test_get_user_by_email():
     user_data: Final = await create_and_login_user()
 
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        response = await ac.get(URL+"/me", headers={"Authorization": f"Bearer {user_data.tokens.token}"})
+        response = await ac.get(URL + f"/{user_data.dto.email}", headers={"Authorization": f"Bearer {user_data.tokens.token}"})
 
     assert response.status_code == 200
 
