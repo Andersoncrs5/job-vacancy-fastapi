5648a02653a66e81d0f32a8d777fbf5b7b653124
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-23 20:40:44 -0300

    set methods

--- app/repositories/base/curriculum_repository_base.py
+++ app/repositories/base/curriculum_repository_base.py
@@ -1,5 +1,26 @@
 from abc import ABC, abstractmethod
+from app.configs.db.database import CurriculumEntity
 
 class CurriculumRepositoryBase(ABC):
+
+    @abstractmethod
+    async def save(self, curri: CurriculumEntity) -> CurriculumEntity:
+        pass
+    
+    @abstractmethod
+    async def add(self, curri: CurriculumEntity) -> CurriculumEntity:
+        pass
+
+    @abstractmethod
+    async def exists_by_user_id(self, user_id: int) -> bool:
+        pass
+
+    @abstractmethod
+    async def delete(self, curri: CurriculumEntity):
+        pass
+
+    @abstractmethod
+    async def get_by_user_id(self, user_id: int) -> CurriculumEntity | None:
+        pass
 
     
