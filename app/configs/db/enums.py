3a00df177639b085c267a0cf66345b97274ac032
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-23 20:00:36 -0300

    more types to MediaType enum

--- app/configs/db/enums.py
+++ app/configs/db/enums.py
@@ -1,8 +1,12 @@
 import enum
 from sqlalchemy import Enum
 
-class MediaType(enum.Enum):
+class MediaType(str, enum.Enum):
     IMAGE = "image"
     VIDEO = "video"
     AUDIO = "audio"
     DOCUMENT = "document"
+    GIF = "gif"
+    PDF = "pdf"
+    TEXT = "text"
+    ARCHIVE = "archive"
