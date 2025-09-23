6e9f6f688ba5ee1faa8969219328a6f9d79490c8
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-22 22:12:04 -0300

    set methods

--- app/repositories/base/media_post_user_repository_base.py
+++ app/repositories/base/media_post_user_repository_base.py
@@ -1,4 +1,26 @@
 from abc import ABC, abstractmethod
 from app.configs.db.database import MediaPostUserEntity
+from app.utils.filter.medias_post_user_filter import MediaPostUserFilter
+from typing import List
 
 class MediaPostUserRepositoryBase(ABC):
+
+    @abstractmethod
+    async def get_all_filter(self, filter: MediaPostUserFilter) -> List[MediaPostUserFilter]:
+        pass
+
+    @abstractmethod
+    async def add(self, media: MediaPostUserEntity) -> MediaPostUserEntity:
+        pass
+
+    @abstractmethod
+    async def save(self, media: MediaPostUserEntity) -> MediaPostUserEntity:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> MediaPostUserEntity | None:
+        pass
+
+    @abstractmethod
+    async def delete(self, media: MediaPostUserEntity):
+        pass
