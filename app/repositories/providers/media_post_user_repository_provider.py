8a37d7335fb4aae878dd347e6021f35a9e0108ce
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-22 22:12:11 -0300

    impls method

--- app/repositories/providers/media_post_user_repository_provider.py
+++ app/repositories/providers/media_post_user_repository_provider.py
@@ -1,3 +1,49 @@
 from app.repositories.base.media_post_user_repository_base import MediaPostUserRepositoryBase
+from app.configs.db.database import MediaPostUserEntity
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func, Result, Select
+from app.utils.filter.medias_post_user_filter import MediaPostUserFilter
+from typing import List, Final
+from datetime import datetime
 
 class MediaPostUserRepositoryProvider(MediaPostUserRepositoryBase):
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_all_filter(self, filter: MediaPostUserFilter) -> List[MediaPostUserFilter]:
+        stmt = select(MediaPostUserFilter)
+
+        stmt = filter.filter(stmt)
+
+        result = await self.db.execute(stmt)
+        all: Final = result.scalars().all()
+        return list(all)
+
+    async def get_by_id(self, id: int) -> MediaPostUserEntity | None:
+        if id <= 0:
+            raise ValueError("id is required")
+
+        stmt = await self.db.execute(
+            select(MediaPostUserEntity).where(MediaPostUserEntity.id == id)
+        )
+
+        return stmt.scalar_one_or_none()
+
+    async def add(self, media: MediaPostUserEntity) -> MediaPostUserEntity:
+        self.db.add(media)
+        await self.db.commit()
+        await self.db.refresh(media)
+
+        return media
+
+    async def save(self, media: MediaPostUserEntity) -> MediaPostUserEntity:
+        media.updated_at = datetime.now()
+        
+        await self.db.commit()
+        await self.db.refresh(media)
+
+        return media
+
+    async def delete(self, media: MediaPostUserEntity):
+        await self.db.delete(media)
+        await self.db.commit()
