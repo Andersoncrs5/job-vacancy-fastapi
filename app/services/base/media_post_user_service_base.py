aeb798a7c695ad9bf8de7f2b955dc223c63cce3d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-23 14:19:42 -0300

    new method

--- app/services/base/media_post_user_service_base.py
+++ app/services/base/media_post_user_service_base.py
@@ -1,15 +1,33 @@
 
 from abc import ABC, abstractmethod
+from typing import List
 
 from app.configs.db.database import MediaPostUserEntity
-
+from app.schemas.media_post_user_schemas import CreateMediaPostUserDTO, UpdateMediaPostUserDTO
+from app.utils.filter.medias_post_user_filter import MediaPostUserFilter
 
 class MediaPostUserServiceBase(ABC):
 
+    @abstractmethod
+    async def get_amount_by_post_id(self, post_id: int) -> int:
+        pass
+
+    @abstractmethod
+    async def update(self, media: MediaPostUserEntity, dto: UpdateMediaPostUserDTO) -> MediaPostUserEntity:
+        pass
+    
+    @abstractmethod
+    async def create(self, post_id: int, dto: CreateMediaPostUserDTO) -> MediaPostUserEntity:
+        pass
+    
+    @abstractmethod
+    async def get_all_filter(self, filter: MediaPostUserFilter) -> List[MediaPostUserFilter]:
+        pass
+    
     @abstractmethod
     async def get_by_id(self, id: int) -> MediaPostUserEntity | None:
         pass
 
     @abstractmethod
-    async def delete(self, id: int):
+    async def delete(self, media: MediaPostUserEntity):
         pass
