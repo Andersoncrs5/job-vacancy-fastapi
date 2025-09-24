dc38b3d045a22a7d43c12e8a34a6d2ce00a9a9d0
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-24 19:10:40 -0300

    set method

--- app/repositories/base/skill_repository_base.py
+++ app/repositories/base/skill_repository_base.py
@@ -1,4 +1,31 @@
 from abc import ABC, abstractmethod
+from uuid import UUID
+from app.configs.db.database import SkillEntity
+from app.utils.filter.skill_filter import SkillFilter
+from typing import List
 
 class SkillRepositoryBase(ABC):
+
+    @abstractmethod
+    async def add(self, skill: SkillEntity) -> SkillEntity:
+        pass
     
+    @abstractmethod
+    async def save(self, skill: SkillEntity) -> SkillEntity:
+        pass
+
+    @abstractmethod
+    async def get_all(self, filter: SkillFilter) -> List[SkillEntity]:
+        pass
+
+    @abstractmethod
+    async def exists_by_name(self, name: str) -> bool:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: UUID) -> (SkillEntity | None):
+        pass
+
+    @abstractmethod
+    async def delete(self, skill: SkillEntity):
+        pass
