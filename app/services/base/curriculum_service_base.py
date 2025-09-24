94ee43cb37c02e6158090dd7295820861d41da3f
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-24 08:46:34 -0300

    set service base to curriculum

---
+++ app/services/base/curriculum_service_base.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from app.configs.db.database import CurriculumEntity
+from app.schemas.curriculum_schemas import CreateCurriculumDTO, UpdateCurriculumDTO
+
+class CurriculumServiceBase(ABC):
+
+    @abstractmethod
+    async def toggle_status_is_updated(self, curri: CurriculumEntity) -> CurriculumEntity:
+        pass
+
+    @abstractmethod
+    async def update(self, curri: CurriculumEntity, dto: UpdateCurriculumDTO) -> CurriculumEntity:
+        pass
+
+    @abstractmethod
+    async def exists_by_user_id(self, user_id: int) -> bool:
+        pass
+    
+    @abstractmethod
+    async def create(self, user_id: int, dto: CreateCurriculumDTO) -> CurriculumEntity:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, user_id: int) -> CurriculumEntity | None:
+        pass
