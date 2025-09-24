c2740d16fa9757578aa34b928684bd50a3c7b652
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-24 11:44:39 -0300

    config logs

---
+++ app/configs/logs/log_config.py
@@ -0,0 +1,21 @@
+import structlog
+import logging
+
+def setup_logging():
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.INFO
+    )
+
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer(),
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    return structlog.get_logger()
