22c88e114d0a42b47000f113040fc81f8fede487
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-25 16:34:24 -0300

    config mapper of UUID to str

--- app/configs/orjson/orjson_config.py
+++ app/configs/orjson/orjson_config.py
@@ -1,8 +1,12 @@
 import orjson
 from pydantic import BaseModel
+from uuid import UUID
 
 class ORJSONModel(BaseModel):
     class Config:
         json_loads = orjson.loads
         json_dumps = lambda v, *, default: orjson.dumps(v, default=default).decode()
         from_attributes = True
+        json_encoders={
+            UUID: lambda v: str(v)  
+        }
