b3c9eb2cbf504e30e9e2d4147b935747e47917c6
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-25 20:19:31 -0300

    set repository base to my skill

---
+++ app/repositories/base/my_skill_repository_base.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from app.configs.db.database import MySkillEntity
+from uuid import UUID
+from app.utils.filter.my_skill_filter import MySkillFilter
+
+class MySkillRepositoryBase(ABC):
+
+    @abstractmethod
+    async def get_all(self, filter: MySkillFilter) -> list[MySkillEntity]: 
+        pass
+
+    @abstractmethod
+    async def save(self, my: MySkillEntity) -> MySkillEntity:
+        pass
+
+    @abstractmethod
+    async def add(self, my: MySkillEntity) -> MySkillEntity:
+        pass
+
+    @abstractmethod
+    async def exists_by_skill_id_and_user_id(self, skill_id: UUID, user_id: int) -> bool:
+        pass
+
+    @abstractmethod
+    async def get_by_skill_id_and_user_id(self, skill_id: UUID, user_id: int) -> MySkillEntity | None:
+        pass
