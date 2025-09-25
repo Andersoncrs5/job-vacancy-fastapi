425a74b8459d4affa4572de233b7185ba9cdca3d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-25 20:56:20 -0300

    new method to_entity and change type of columns

--- app/schemas/my_skill_schemas.py
+++ app/schemas/my_skill_schemas.py
@@ -2,6 +2,7 @@ from app.configs.orjson.orjson_config import ORJSONModel
 from uuid import UUID
 from datetime import datetime, date
 from app.configs.db.enums import ProficiencyEnum
+from app.configs.db.database import MySkillEntity
 
 class MySkillOUT(ORJSONModel):
     user_id: int
@@ -18,13 +19,25 @@ class UpdateMySkillDTO(ORJSONModel):
     certificate_url: str | None
     datails: str | None
     years_of_experience: int | None
-    last_used_date: date
+    last_used_date: date | None
 
 class CreateMySkillDTO(ORJSONModel):
-    skill_id: UUID | None
-    proficiency: ProficiencyEnum | None
+    skill_id: UUID
+    proficiency: ProficiencyEnum
     certificate_url: str | None
     datails: str | None
     years_of_experience: int | None
-    last_used_date: date | None
+    last_used_date: date
+
+    def to_entity(self):
+        from app.configs.db.database import MySkillEntity
+
+        return MySkillEntity(
+            skill_id = self.skill_id,
+            proficiency = self.proficiency,
+            certificate_url = self.certificate_url,
+            datails = self.datails,
+            years_of_experience = self.years_of_experience,
+            last_used_date = self.last_used_date,
+        )
 
