72137442f9c41985d0a609b90df63becdf50cdf9
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-26 15:10:42 -0300

    config serializer to date and datetime

--- app/configs/orjson/orjson_config.py
+++ app/configs/orjson/orjson_config.py
@@ -1,12 +1,20 @@
 import orjson
 from pydantic import BaseModel
 from uuid import UUID
+from datetime import date, datetime
+
+def orjson_default(obj):
+    if isinstance(obj, UUID):
+        return obj.hex 
+    if isinstance(obj, (date, datetime)):
+        return obj.isoformat()
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+def orjson_dumps(v, *, default):
+    return orjson.dumps(v, default=default).decode()
 
 class ORJSONModel(BaseModel):
     class Config:
         json_loads = orjson.loads
-        json_dumps = lambda v, *, default: orjson.dumps(v, default=default).decode()
+        json_dumps = orjson_dumps
         from_attributes = True
-        json_encoders={
-            UUID: lambda v: str(v)  
-        }
