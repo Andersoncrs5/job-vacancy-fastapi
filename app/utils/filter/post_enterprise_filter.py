2b9e559f8cf845e85b4eafefda880501655777b4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-27 09:42:33 -0300

    set filter to post enterprise

---
+++ app/utils/filter/post_enterprise_filter.py
@@ -0,0 +1,13 @@
+from fastapi_filter.contrib.sqlalchemy import Filter
+from pydantic import Field
+from typing import Optional
+from datetime import datetime
+from app.configs.db.database import PostEnterpriseEntity
+
+class PostEnterpriseFilter(Filter):
+    title__ilike: Optional[str] = Field(None, description="Search by title (case-insensitive).")
+    created_at__gte: Optional[datetime] = Field(None, description="Filter by creation date, greater than or equal to.")
+    created_at__lte: Optional[datetime] = Field(None, description="Filter by creation date, less than or equal to.")
+
+    class Constants(Filter.Constants):
+        model = PostEnterpriseEntity
