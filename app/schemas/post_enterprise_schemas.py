7f0d6ed42f3c2ea9814cbb3d3bc0aceebda946d9
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-27 09:33:38 -0300

    set schemas to enterprise post

---
+++ app/schemas/post_enterprise_schemas.py
@@ -0,0 +1,31 @@
+from app.configs.orjson.orjson_config import ORJSONModel
+from datetime import datetime
+
+class PostEnterpriseOUT(ORJSONModel):
+    id: int
+    title: str
+    content: str
+    url_image: str | None
+    Enterprise_id: int
+    category_id: int
+    created_at: datetime | str
+    updated_at: datetime | str
+
+class CreatePostEnterpriseDTO(ORJSONModel):
+    title: str
+    content: str
+    url_image: str | None
+
+    def to_entity(self):
+        from app.configs.db.database import PostEnterpriseEntity
+
+        return PostEnterpriseEntity(
+            title = self.title,
+            content = self.content,
+            url_image = self.url_image,
+        )
+
+class UpdatePostEnterpriseDTO(ORJSONModel):
+    title: str | None
+    content: str | None
+    url_image: str | None
