67c545a94f516a8dff0d417e8c69c0bef8fee804
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-28 10:02:07 -0300

    new methods update, create, delete, exists_by_user_id and exists_by_id

--- app/services/base/employee_enterprise_service_base.py
+++ app/services/base/employee_enterprise_service_base.py
@@ -1,7 +1,28 @@
 from abc import ABC, abstractmethod
 from app.configs.db.database import EmployeeEnterpriseEntity
+from app.schemas.employee_enterprise_schemas import CreateEmployeeEnterpriseDTO, UpdateEmployeeEnterpriseDTO
 
 class EmployeeEnterpriseServiceBase(ABC):
+
+    @abstractmethod
+    async def update(self, emp: EmployeeEnterpriseEntity, dto: UpdateEmployeeEnterpriseDTO) -> EmployeeEnterpriseEntity:
+        pass
+
+    @abstractmethod
+    async def create(self, user_id: int, dto: CreateEmployeeEnterpriseDTO) -> EmployeeEnterpriseEntity:
+        pass
+
+    @abstractmethod
+    async def delete(self, emp: EmployeeEnterpriseEntity):
+        pass
+
+    @abstractmethod
+    async def exists_by_user_id(self, user_id: int) -> bool:
+        pass
+
+    @abstractmethod
+    async def exists_by_id(self, id: int) -> bool:
+        pass
 
     @abstractmethod
     async def get_by_id(self, id: int) -> EmployeeEnterpriseEntity | None:
