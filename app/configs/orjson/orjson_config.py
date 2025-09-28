38f89596396a24c5d45328c0f71fbedaf8f1f03d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-28 15:03:25 -0300

    remake class ORJSONModel

--- app/configs/orjson/orjson_config.py
+++ app/configs/orjson/orjson_config.py
@@ -1,23 +1,24 @@
 import orjson
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from uuid import UUID
 from datetime import date, datetime
 import json
 
+
 def orjson_default(obj):
     if isinstance(obj, UUID):
-        return str(obj)          
-
+        return str(obj)
     if isinstance(obj, (date, datetime)):
         return str(obj)
-
     return json.JSONEncoder().default(obj)
+
 
 def orjson_dumps(v, *, default=orjson_default):
     return orjson.dumps(v, default=default).decode()
 
+
 class ORJSONModel(BaseModel):
-    class Config:
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
+
+    def model_dump_json(self, **kwargs):
+        return orjson_dumps(self.model_dump(**kwargs))
