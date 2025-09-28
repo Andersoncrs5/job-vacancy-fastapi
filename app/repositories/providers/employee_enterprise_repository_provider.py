0dbc6922d8514e5f0e9454d60906cb46101baed2
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-27 21:52:54 -0300

    impl method get_by_id and delete

--- app/repositories/providers/employee_enterprise_repository_provider.py
+++ app/repositories/providers/employee_enterprise_repository_provider.py
@@ -1,3 +1,22 @@
 from app.repositories.base.employee_enterprise_repository_base import EmployeeEnterpriseRepositoryBase
+from app.configs.db.database import EmployeeEnterpriseEntity
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func, Result
 
 class EmployeeEnterpriseRepositoryProvider(EmployeeEnterpriseRepositoryBase):
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_by_id(self, id: int) -> EmployeeEnterpriseEntity | None:
+        if id <= 0:
+            raise ValueError("Id is null or less than 0")
+
+        stmt = select(EmployeeEnterpriseEntity).where(EmployeeEnterpriseEntity.id == id)
+
+        result = await self.db.execute(stmt)
+
+        return result.scalars().first()
+
+    async def delete(self, emp: EmployeeEnterpriseEntity):
+        await self.db.delete(emp)
+        await self.db.commit()
