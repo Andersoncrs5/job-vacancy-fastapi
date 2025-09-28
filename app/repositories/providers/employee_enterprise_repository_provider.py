8be4a8e1be095760a51cb2f349991471cc9f2f93
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-27 22:07:12 -0300

    impl method save, add and get_all

--- app/repositories/providers/employee_enterprise_repository_provider.py
+++ app/repositories/providers/employee_enterprise_repository_provider.py
@@ -2,10 +2,36 @@ from app.repositories.base.employee_enterprise_repository_base import EmployeeEn
 from app.configs.db.database import EmployeeEnterpriseEntity
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, func, Result
+from datetime import datetime
+from typing import Final
+from app.utils.filter.employee_enterprise_filter import EmployeeEnterpriseFilter
 
 class EmployeeEnterpriseRepositoryProvider(EmployeeEnterpriseRepositoryBase):
     def __init__(self, db: AsyncSession):
         self.db = db
+
+    async def get_all(self, filter: EmployeeEnterpriseFilter) -> list[EmployeeEnterpriseEntity]:
+        stmt = select(EmployeeEnterpriseEntity)
+
+        stmt = filter.filter(stmt)
+
+        result: Final = await self.db.execute(stmt)
+        all: Final = result.scalars().all()
+        return list(all)
+
+    async def save(self, emp: EmployeeEnterpriseEntity) -> EmployeeEnterpriseEntity:
+        emp.updated_at = datetime.now()
+        await self.db.commit()
+        await self.db.refresh(emp)
+
+        return emp
+
+    async def add(self, emp: EmployeeEnterpriseEntity) -> EmployeeEnterpriseEntity:
+        self.db.add(emp)
+        await self.db.commit()
+        await self.db.refresh(emp)
+
+        return emp
 
     async def get_by_id(self, id: int) -> EmployeeEnterpriseEntity | None:
         if id <= 0:
