0fb64053f6cecf21ffe06354b700ac4bfc6b4413
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-27 21:52:46 -0300

    new methods get_by_id and delete

--- app/repositories/base/employee_enterprise_repository_base.py
+++ app/repositories/base/employee_enterprise_repository_base.py
@@ -1,5 +1,12 @@
 from abc import ABC, abstractmethod
+from app.configs.db.database import EmployeeEnterpriseEntity
 
 class EmployeeEnterpriseRepositoryBase(ABC):
 
-    
+    @abstractmethod
+    async def get_by_id(self, id: int) -> EmployeeEnterpriseEntity | None:
+        pass
+
+    @abstractmethod
+    async def delete(self, emp: EmployeeEnterpriseEntity):
+        pass
