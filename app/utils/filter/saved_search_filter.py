396ca1700ebdfb9dffe36c4aebdd8b1bcc1aabb5
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-29 20:34:58 -0300

    set filter to SavedSearchEntity

---
+++ app/utils/filter/saved_search_filter.py
@@ -0,0 +1,14 @@
+from fastapi_filter.contrib.sqlalchemy import Filter
+from pydantic import Field
+from typing import Optional
+from datetime import datetime
+from app.configs.db.database import SavedSearchEntity
+
+class SavedSearchFilter(Filter):
+    name__ilike: Optional[str] = Field(None, description="Search by name (case-insensitive).")
+    description__ilike: Optional[str] = Field(None, description="Search by description (case-insensitive).")
+    created_at__gte: Optional[datetime] = Field(None, description="Filter by creation date, greater than or equal to.")
+    created_at__lte: Optional[datetime] = Field(None, description="Filter by creation date, less than or equal to.")
+
+    class Constants(Filter.Constants):
+        model = SavedSearchEntity
