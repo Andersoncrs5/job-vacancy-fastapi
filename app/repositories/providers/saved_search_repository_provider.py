1824b90888271a6aecb848ada80181712436536f
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-29 20:47:58 -0300

    config SavedSearchReposioryProvider

---
+++ app/repositories/providers/saved_search_repository_provider.py
@@ -0,0 +1,6 @@
+from app.repositories.base.saved_search_repository_base import SavedSearchReposioryBase
+from app.configs.db.database import SavedSearchEntity
+from app.utils.filter.saved_search_filter import SavedSearchFilter
+from typing import List
+
+class SavedSearchReposioryProvider(SavedSearchReposioryBase):
