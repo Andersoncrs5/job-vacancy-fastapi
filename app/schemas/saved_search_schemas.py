a3476ff6fd43596ed66844b272f93b7102aba0bd
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-29 20:40:27 -0300

    set schemas update, create and out to SavedSearchEntity

---
+++ app/schemas/saved_search_schemas.py
@@ -0,0 +1,34 @@
+from app.configs.orjson.orjson_config import ORJSONModel
+from datetime import datetime
+from app.configs.db.database import SavedSearchEntity
+
+class SavedSearchOUT(ORJSONModel):
+    id: int
+    user_id: int
+    name: str
+    query: dict
+    description: str | None
+    is_public: bool
+    last_executed_at: datetime | str | None
+    execution_count: int
+    notifications_enabled: bool
+    created_at: datetime | str
+    updated_at: datetime | str
+
+class CreateSavedSearchOUT(ORJSONModel):
+    name: str
+    query: dict
+    description: str | None
+    is_public: bool
+    last_executed_at: datetime | None
+    execution_count: int
+    notifications_enabled: bool
+
+class UpdateSavedSearchOUT(ORJSONModel):
+    name: str | None
+    query: dict | None
+    description: str | None
+    is_public: bool | None
+    last_executed_at: datetime | None
+    execution_count: int | None
+    notifications_enabled: bool | None
