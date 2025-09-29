e3ac7815cc927bf5780faa1711cd395b3515ddf1
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-29 20:56:56 -0300

    new method

--- app/repositories/base/saved_search_repository_base.py
+++ app/repositories/base/saved_search_repository_base.py
@@ -4,4 +4,27 @@ from app.utils.filter.saved_search_filter import SavedSearchFilter
 from typing import List
 
 class SavedSearchReposioryBase(ABC):
-    
+
+    @abstractmethod
+    async def get_all(self, filter: SavedSearchFilter) -> List[SavedSearchEntity]:
+        pass
+
+    @abstractmethod
+    async def delete(self, save: SavedSearchEntity):
+        pass
+
+    @abstractmethod
+    async def add(self, save: SavedSearchEntity) -> SavedSearchEntity:
+        pass
+
+    @abstractmethod
+    async def save(self, save: SavedSearchEntity) -> SavedSearchEntity:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> SavedSearchEntity | None:
+        pass
+
+    @abstractmethod
+    async def exists_by_id(self, id: int) -> bool:
+        pass
