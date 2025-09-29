589d2a3a3f508aa72b272258c0fc9e1928e2deb9
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-29 20:42:22 -0300

    config SavedSearchReposioryBase

---
+++ app/repositories/base/saved_search_repository_base.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+from app.configs.db.database import SavedSearchEntity
+from app.utils.filter.saved_search_filter import SavedSearchFilter
+from typing import List
+
+class SavedSearchReposioryBase(ABC):
+    
