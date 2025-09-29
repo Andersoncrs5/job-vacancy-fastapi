ea14cfc45d449e96e36e5d68cb67f231998ebd7a
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-29 13:37:16 -0300

    new method get_all, exists_by_id and exists_by_user_id

--- app/repositories/base/review_enterprise_repository_base.py
+++ app/repositories/base/review_enterprise_repository_base.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
-
+from typing import List
 from app.configs.db.database import ReviewEnterprise
-
+from app.utils.filter.review_enterprise_filter import ReviewEnterpriseFilter
 
 class ReviewEnterpriseRepositoryBase(ABC):
+
+    @abstractmethod
+    async def get_all(self, filter: ReviewEnterpriseFilter) -> List[ReviewEnterprise]:
+        pass
 
     @abstractmethod
     async def add(self, view: ReviewEnterprise):
@@ -15,6 +19,14 @@ class ReviewEnterpriseRepositoryBase(ABC):
 
     @abstractmethod
     async def delete(self, view: ReviewEnterprise):
+        pass
+
+    @abstractmethod
+    async def exists_by_id(self, id: int) -> bool:
+        pass
+
+    @abstractmethod
+    async def exists_by_user_id(self, id: int) -> bool:
         pass
 
     @abstractmethod
