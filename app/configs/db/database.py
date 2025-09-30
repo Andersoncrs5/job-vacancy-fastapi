37ca2725d3d3632439d619bdb34536ba010832aa
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-30 20:02:57 -0300

    new column is_block to user entity

--- app/configs/db/database.py
+++ app/configs/db/database.py
@@ -45,6 +45,7 @@ class UserEntity(Base):
     bio: Mapped[str | None] = mapped_column(Text, nullable=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_block: Mapped[bool] = mapped_column(Boolean, default=False)
     avatar_url: Mapped[str | None] = mapped_column(String(255), nullable=True)
     refresh_token: Mapped[str | None] = mapped_column(String(), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
