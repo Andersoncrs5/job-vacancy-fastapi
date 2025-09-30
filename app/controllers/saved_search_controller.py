497d672bdc94960f9f10b9d9e94398423a0593a9
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-30 13:55:14 -0300

    config save search controller

---
+++ app/controllers/saved_search_controller.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import ORJSONResponse
+from app.configs.db.database import SavedSearchEntity, UserEntity
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from typing import Final
+from app.utils.res.response_body import ResponseBody
+from app.utils.res.responses_http import *
+from app.schemas.saved_search_schemas import *
+from app.services.providers.user_service_provider import UserServiceProvider
+from app.dependencies.service_dependency import *
+from fastapi_pagination import Page, add_pagination, paginate
+from datetime import datetime
+
+router: Final[APIRouter] = APIRouter(
+    prefix="/api/v1/saved-search", 
+    tags=["Saved Search"],
+    responses={
+        500: RESPONSE_500,
+        status.HTTP_401_UNAUTHORIZED: RESPONSE_401,
+    },
+    deprecated=False,
+)
+
+bearer_scheme: Final[HTTPBearer] = HTTPBearer()
+
+
+
+add_pagination(router)
