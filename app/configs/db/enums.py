54018127fb33643bd80d1bbc1b48fc6d83e81b5d
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-30 19:45:42 -0300

    new enums ExperienceLevelEnum, EducationLevelEnum, VacancyStatusEnum, CurrencyEnum

--- app/configs/db/enums.py
+++ app/configs/db/enums.py
@@ -33,3 +33,29 @@ class EmploymentStatusEnum(str, enum.Enum):
     on_leave = "on_leave"   
     vacation = "vacation"   
     probation = "probation" 
+
+class ExperienceLevelEnum(str, enum.Enum):
+    INTERN = "Internship"
+    JUNIOR = "Junior"
+    PLENO = "Pleno"
+    SENIOR = "Senior"
+    LEAD = "Lead"
+
+class EducationLevelEnum(str, enum.Enum):
+    NONE = "None"
+    HIGH_SCHOOL = "HighSchool"
+    TECHNICAL = "Technical"
+    BACHELOR = "Bachelor"
+    MASTER = "Master"
+    DOCTORATE = "Doctorate"
+
+class VacancyStatusEnum(str, enum.Enum):
+    OPEN = "Open"
+    CLOSED = "Closed"
+    PAUSED = "Paused"
+
+class CurrencyEnum(str, enum.Enum):
+    BRL = "BRL"
+    USD = "USD"
+    EUR = "EUR"
+    BTC = "BTC"
