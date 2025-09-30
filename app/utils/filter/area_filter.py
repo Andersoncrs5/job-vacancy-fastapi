61dbc43de4cf9c16edeb8e5a70f895877846e593
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-30 20:15:43 -0300

    mew filter to AreaEntity

---
+++ app/utils/filter/area_filter.py
@@ -0,0 +1,13 @@
+from fastapi_filter.contrib.sqlalchemy import Filter
+from pydantic import Field
+from typing import Optional
+from datetime import date, datetime
+from app.configs.db.database import AreaEntity
+
+class AreaFilter(Filter):
+    name__ilike: Optional[str] = Field(None, description="Search by name (case-insensitive).")
+    created_at__gte: Optional[datetime] = Field(None, description="Filter by creation date, greater than or equal to.")
+    created_at__lte: Optional[datetime] = Field(None, description="Filter by creation date, less than or equal to.")
+
+    class Constants(Filter.Constants):
+        model = AreaEntity
