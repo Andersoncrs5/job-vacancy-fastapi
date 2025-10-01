c71acec55822c68c9c7f8df074fceab8a6ca7666
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-01 14:00:17 -0300

    configed vacancy service base and your method

---
+++ app/services/base/vacancy_service_base.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from app.configs.db.database import VacancyEntity
+from app.utils.filter.vacancy_filter import VacancyFilter
+from typing import List
+from app.schemas.vacancy_schemas import CreateVacancyDTO, UpdateVacancyDTO
+
+class VacancyServiceBase(ABC):
+
+    @abstractmethod
+    async def get_all(self, filter: VacancyFilter) -> List[VacancyEntity]:
+        pass
+
+    @abstractmethod
+    async def update(self, vacancy: VacancyEntity, dto: UpdateVacancyDTO) -> VacancyEntity:
+        pass
+    
+    @abstractmethod
+    async def create(self, enterprise_id: int, dto: CreateVacancyDTO) -> VacancyEntity:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> VacancyEntity | None:
+        pass
+
+    @abstractmethod
+    async def exists_by_id(self, id: int) -> bool:
+        pass
