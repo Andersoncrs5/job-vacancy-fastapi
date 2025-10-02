0ed8b7cf1f82f03a22015f1ea25d53270a275327
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-02 14:21:37 -0300

    new classes to create update vacancy skill

--- app/schemas/vacancy_skill_schemas.py
+++ app/schemas/vacancy_skill_schemas.py
@@ -1,5 +1,4 @@
 from app.configs.orjson.orjson_config import ORJSONModel
-from datetime import datetime
 from app.configs.db.enums import ProficiencyEnum
 from app.schemas.skill_schemas import SkillOUT
 
@@ -11,3 +10,31 @@ class VacancySkillOUT(ORJSONModel):
     notes: str | None
     skill: SkillOUT
     
+class CreateVacancySkillDTO(ORJSONModel):
+    vacancy_id: int
+    skill_id: int
+    is_required: bool
+    proficiency: ProficiencyEnum | None
+    years_experience: int | None
+    priority_level: int | None
+    notes: str | None
+
+    def to_entity(self):
+        from app.configs.db.database import VacancySkillEntity
+
+        return VacancySkillEntity(
+            is_required = self.is_required,
+            proficiency = self.proficiency,
+            years_experience = self.years_experience,
+            priority_level = self.priority_level,
+            notes = self.notes,
+        )
+
+
+class UpdateVacancySkillDTO(ORJSONModel):
+    is_required: bool | None
+    proficiency: ProficiencyEnum | None
+    years_experience: int | None
+    priority_level: int | None
+    notes: str | None
+
