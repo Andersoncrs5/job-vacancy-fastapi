0352093596d83d7a60562ab2d03f148f6d04dc0b
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-03 21:35:46 -0300

    set routers of address_user_controller

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from app.controllers import (
     skill_controller, my_skill_controller, post_enterprise_controller,
     favorite_post_enterprise_controller, employee_enterprise_controller,
     review_enterprise_controller, saved_search_controller, area_controller,
-    vacancy_controller, vacancy_skill_controller
+    vacancy_controller, vacancy_skill_controller, address_user_controller
 )
 from app.configs.db.database import get_db, engine, Base
 from contextlib import asynccontextmanager
@@ -55,6 +55,7 @@ async def add_request_id(request, call_next):
     response = await call_next(request)
     return response
 
+app.include_router(address_user_controller.router)
 app.include_router(vacancy_skill_controller.router)
 app.include_router(vacancy_controller.router)
 app.include_router(area_controller.router)
