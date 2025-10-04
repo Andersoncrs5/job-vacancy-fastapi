245f4f456fb922330bf1f26d48f0583f10f2b363
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-03 21:36:29 -0300

    impl controller to address user

---
+++ app/controllers/address_user_controller.py
@@ -0,0 +1,375 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import ORJSONResponse
+from app.configs.db.database import AddressUserEntity, UserEntity
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from typing import Final
+from app.utils.res.response_body import ResponseBody
+from app.utils.res.responses_http import *
+from app.schemas.address_user_schemas import *
+from app.services.providers.user_service_provider import UserServiceProvider
+from app.services.providers.address_user_service_provider import AddressUserServiceProvider
+from app.dependencies.service_dependency import *
+from fastapi_pagination import Page, add_pagination, paginate
+from datetime import datetime
+
+router: Final[APIRouter] = APIRouter(
+    prefix="/api/v1/address-user", 
+    tags=["address-user"],
+    responses={
+        500: RESPONSE_500,
+        status.HTTP_401_UNAUTHORIZED: RESPONSE_401,
+    },
+    deprecated=False,
+)
+
+bearer_scheme: Final[HTTPBearer] = HTTPBearer()
+
+@router.get(
+    "/{user_id}/exists",
+    status_code=200,
+    response_model=ResponseBody[None],   
+    responses={
+        400: RESPONSE_400_ID_REQUIRED,
+    }
+)
+async def exists_by_id(
+    user_id: int,
+    user_service: UserServiceProvider = Depends(get_user_service_provider_dependency),
+    address_service: AddressUserServiceProvider = Depends(get_address_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if user_id <= 0:
+        return ORJSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_400_BAD_REQUEST,
+                    message="Id is required",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        exists: Final = await address_service.exists_by_user_id(user_id)
+
+        return ORJSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[bool](
+                message="",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=exists,
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+
+@router.patch(
+    "",
+    status_code=200,
+    response_model=ResponseBody[None],   
+    responses={
+        404: RESPONSE_404,
+        400: RESPONSE_400,
+    }
+)
+async def patch(
+    dto: UpdateAddressUserDTO,
+    user_service: UserServiceProvider = Depends(get_user_service_provider_dependency),
+    address_service: AddressUserServiceProvider = Depends(get_address_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+        user_id: Final[int] = jwt_service.extract_user_id_v2(token)
+
+        address: Final = await address_service.get_by_user_id(user_id)
+        if address is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Address not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        address_updated = await address_service.update(address, dto)
+
+        out = address_updated.to_out()
+
+        return ORJSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[dict](
+                message="Address updated with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=dict(out),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.delete(
+    "",
+    status_code=200,
+    response_model=ResponseBody[None],   
+    responses={
+        404: RESPONSE_404,
+        400: RESPONSE_400,
+    }
+)
+async def delete(
+    user_service: UserServiceProvider = Depends(get_user_service_provider_dependency),
+    address_service: AddressUserServiceProvider = Depends(get_address_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+        user_id: Final[int] = jwt_service.extract_user_id_v2(token)
+
+        address: Final = await address_service.get_by_user_id(user_id)
+        if address is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Address not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        await address_service.delete(address)
+
+        return ORJSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[None](
+                message="Address deleted with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=None,
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.get(
+    "/{user_id}",
+    status_code=200,
+    response_model=ResponseBody[None],   
+    responses={
+        404: RESPONSE_404_CATEGORY,
+        400: RESPONSE_400_ID_REQUIRED,
+    }
+)
+async def get_by_id(
+    user_id: int,
+    user_service: UserServiceProvider = Depends(get_user_service_provider_dependency),
+    address_service: AddressUserServiceProvider = Depends(get_address_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if user_id <= 0:
+        return ORJSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_400_BAD_REQUEST,
+                    message="Id is required",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        address: Final = await address_service.get_by_user_id(user_id)
+        if address is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Address not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        out = address.to_out()
+
+        return ORJSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[dict](
+                message="Address found with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=dict(out),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.post(
+    '',
+    status_code=200,
+    response_model=ResponseBody[AddressUserOUT],   
+    responses={
+        400: RESPONSE_400,
+    }
+)
+async def create(
+    dto: CreateAddressUserDTO,
+    user_service: UserServiceProvider = Depends(get_user_service_provider_dependency),
+    address_service: AddressUserServiceProvider = Depends(get_address_user_service_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+        user_id: Final[int] = jwt_service.extract_user_id_v2(token)
+
+        check: Final[bool] = await address_service.exists_by_user_id(user_id)
+        if check == True:
+            return ORJSONResponse(
+                status_code=409,
+                content=dict(ResponseBody[None](
+                    code=409,
+                    message=f"You already have a address",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        user: Final[UserEntity | None] = await user_service.get_by_id(user_id)
+        if user is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody[None](
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="User not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+ 
+        address_created: Final[AddressUserEntity] = await address_service.create(user.id, dto)
+
+        address_mapped: Final = address_created.to_out()
+
+        return ORJSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content=dict(ResponseBody[dict](
+                message="Address user created with successfully",
+                code=status.HTTP_201_CREATED,
+                status=True,
+                body=dict(address_mapped),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
