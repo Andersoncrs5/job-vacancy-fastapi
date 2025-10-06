ef48b15bbc24950874010cf6186f1402d7c91794
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-06 15:47:05 -0300

    Impl repository base to follow system

---
+++ app/repositories/base/follow_repository_base.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+from app.configs.db.database import FollowerRelationshipEntity
+
+class FollowRepositoryBase(ABC):
+
+    @abstractmethod
+    async def get_all(self, follower_id: int) -> list[FollowerRelationshipEntity] :
+        pass
+
+    @abstractmethod
+    async def add(self, follow: FollowerRelationshipEntity) -> FollowerRelationshipEntity:
+        pass
+
+    @abstractmethod
+    async def exists_by_follower_id_and_followed_id(self, follower_id: int, followed_id: int) -> bool:
+        pass
+
+    @abstractmethod
+    async def get_by_follower_id_and_followed_id(self, follower_id: int, followed_id: int) -> FollowerRelationshipEntity | None:
+        pass
+
+    @abstractmethod
+    async def delete(self, follow: FollowerRelationshipEntity):
+        pass
