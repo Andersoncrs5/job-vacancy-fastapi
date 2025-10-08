d0802dba7f5893b19de40b167f631aadfaf722f1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-08 13:12:24 -0300

    Impl methods abstract

--- app/repositories/base/comment_post_user_repository_base.py
+++ app/repositories/base/comment_post_user_repository_base.py
@@ -1,4 +1,32 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from app.configs.db.database import CommentPostUserEntity
+from app.utils.filter.comment_post_user_filter import CommentPostUserFilter
 
 
 class CommentPostUserRepositoryBase(ABC):
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> CommentPostUserEntity | None:
+        pass
+
+    @abstractmethod
+    async def delete(self, comment: CommentPostUserEntity):
+        pass
+
+    @abstractmethod
+    async def get_all(self, filter: CommentPostUserFilter) -> list[CommentPostUserEntity]:
+        pass
+
+    @abstractmethod
+    async def add(self, comment: CommentPostUserEntity) -> CommentPostUserEntity:
+        pass
+
+    @abstractmethod
+    async def save(self, comment: CommentPostUserEntity) -> CommentPostUserEntity:
+        pass
+
+    @abstractmethod
+    async def exists_by_id(self, id: int) -> bool:
+        pass
+
