1ccde65ad611ab711d1f20d44ca18dc626cae921
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-08 20:12:03 -0300

    Impl integration test to comment_post_enterprise_controller

---
+++ tests/integration/test_comment_post_enterprise_controller.py
@@ -0,0 +1,441 @@
+from fastapi.testclient import TestClient
+from typing import Final
+
+from app.schemas.comment_post_enterprise_schemas import CreateCommentPostEnterpriseDTO, UpdateCommentPostEnterpriseDTO
+from main import app
+from httpx import ASGITransport, AsyncClient
+import pytest
+import random
+
+from tests.integration.helper import create_and_login_user, create_category, create_post_enterprise, create_enterprise, \
+    create_industry, create_comment_enterprise_user
+
+client: Final[TestClient] = TestClient(app)
+URL: Final[str] = "/api/v1/comment-post-enterprise"
+
+@pytest.mark.asyncio
+async def test_patch_comment():
+    user_data = await create_and_login_user()
+    category_data: Final = await create_category(user_data)
+    industry_data = await create_industry(user_data)
+    enterprise_data = await create_enterprise(user_data, industry_data)
+    post_enterprise_data: Final = await create_post_enterprise(user_data, enterprise_data, category_data)
+    comment_data = await create_comment_enterprise_user(user_data, post_enterprise_data)
+
+    dto = UpdateCommentPostEnterpriseDTO(
+        content = "update" * 50
+    )
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.patch(
+            f"{URL}/{comment_data.id}",
+            json=dict(dto),
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 200
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 200
+    assert data['message'] == "Comment updated with successfully"
+    assert data['status'] == True
+
+    assert body is not None
+    assert body['id'] == comment_data.id
+    assert body['content'] == dto.content
+
+@pytest.mark.asyncio
+async def test_not_found_patch_comment():
+    user_data = await create_and_login_user()
+
+    dto = UpdateCommentPostEnterpriseDTO(
+        content = "update" * 50
+    )
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.patch(
+            f"{URL}/{999999999}",
+            json=dict(dto),
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 404
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 404
+    assert data['message'] == "Comment not found"
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_bad_request_patch_comment():
+    user_data = await create_and_login_user()
+
+    dto = UpdateCommentPostEnterpriseDTO(
+        content = "update" * 50
+    )
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.patch(
+            f"{URL}/{0}",
+            json=dict(dto),
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 400
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 400
+    assert data['message'] == "Id is required"
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_not_found_delete_comment_create_comment():
+    user_data = await create_and_login_user()
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.delete(
+            f"{URL}/{99999999}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 404
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 404
+    assert data['message'] == "Comment not found"
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_delete_comment_create_comment():
+    user_data = await create_and_login_user()
+    category_data: Final = await create_category(user_data)
+    industry_data = await create_industry(user_data)
+    enterprise_data = await create_enterprise(user_data, industry_data)
+    post_enterprise_data: Final = await create_post_enterprise(user_data, enterprise_data, category_data)
+    comment_data = await create_comment_enterprise_user(user_data, post_enterprise_data)
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.delete(
+            f"{URL}/{comment_data.id}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 200
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 200
+    assert data['message'] == "Comment deleted with successfully"
+    assert data['status'] == True
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_bad_request_delete_comment_create_comment():
+    user_data = await create_and_login_user()
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.delete(
+            f"{URL}/{0}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 400
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 400
+    assert data['message'] == "Id is required"
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_not_found_delete_comment_create_comment():
+    user_data = await create_and_login_user()
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.delete(
+            f"{URL}/{99999999}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 404
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 404
+    assert data['message'] == "Comment not found"
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_delete_comment_create_comment():
+    user_data = await create_and_login_user()
+    category_data: Final = await create_category(user_data)
+    industry_data = await create_industry(user_data)
+    enterprise_data = await create_enterprise(user_data, industry_data)
+    post_enterprise_data: Final = await create_post_enterprise(user_data, enterprise_data, category_data)
+    comment_data = await create_comment_enterprise_user(user_data, post_enterprise_data)
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.delete(
+            f"{URL}/{comment_data.id}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 200
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 200
+    assert data['message'] == "Comment deleted with successfully"
+    assert data['status'] == True
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_bad_request_get_comment_create_comment():
+    user_data = await create_and_login_user()
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.get(
+            f"{URL}/{0}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 400
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 400
+    assert data['message'] == "Id is required"
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_not_found_get_comment_create_comment():
+    user_data = await create_and_login_user()
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.get(
+            f"{URL}/{99999999}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 404
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 404
+    assert data['message'] == "Comment not found"
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_get_comment_create_comment():
+    user_data = await create_and_login_user()
+    category_data: Final = await create_category(user_data)
+    industry_data = await create_industry(user_data)
+    enterprise_data = await create_enterprise(user_data, industry_data)
+    post_enterprise_data: Final = await create_post_enterprise(user_data, enterprise_data, category_data)
+    comment_data = await create_comment_enterprise_user(user_data, post_enterprise_data)
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.get(
+            f"{URL}/{comment_data.id}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 200
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 200
+    assert data['message'] == "Comment found with successfully"
+    assert data['status'] == True
+
+    assert body['id'] == comment_data.id
+    assert body['user_id'] == user_data.out.id
+    assert body['post_enterprise_id'] == post_enterprise_data.id
+
+@pytest.mark.asyncio
+async def test_not_found_post_create_on_comment_create_comment():
+    user_data = await create_and_login_user()
+    category_data: Final = await create_category(user_data)
+    industry_data = await create_industry(user_data)
+    enterprise_data = await create_enterprise(user_data, industry_data)
+
+    dto = CreateCommentPostEnterpriseDTO(
+        content = ("abc" * 50),
+        post_enterprise_id = 9999999999,
+        parent_comment_id = None
+    )
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.post(
+            f"{URL}",
+            json=dict(dto),
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 404
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 404
+    assert data['message'] == 'Post not found'
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_not_found_comment_create_on_comment_create_comment():
+    user_data = await create_and_login_user()
+    category_data: Final = await create_category(user_data)
+    industry_data = await create_industry(user_data)
+    enterprise_data = await create_enterprise(user_data, industry_data)
+    post_enterprise_data: Final = await create_post_enterprise(user_data, enterprise_data, category_data)
+
+    dto = CreateCommentPostEnterpriseDTO(
+        content = ("abc" * 50),
+        post_enterprise_id = post_enterprise_data.id,
+        parent_comment_id = 999999999
+    )
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.post(
+            f"{URL}",
+            json=dict(dto),
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 404
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 404
+    assert data['message'] == 'Comment not found'
+    assert data['status'] == False
+
+    assert body is None
+
+@pytest.mark.asyncio
+async def test_create_on_comment_create_comment():
+    user_data = await create_and_login_user()
+    category_data: Final = await create_category(user_data)
+    industry_data = await create_industry(user_data)
+    enterprise_data = await create_enterprise(user_data, industry_data)
+    post_enterprise_data: Final = await create_post_enterprise(user_data, enterprise_data, category_data)
+    comment_data = await create_comment_enterprise_user(user_data, post_enterprise_data)
+
+    dto = CreateCommentPostEnterpriseDTO(
+        content = ("abc" * 50),
+        post_enterprise_id = post_enterprise_data.id,
+        parent_comment_id = comment_data.id
+    )
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.post(
+            f"{URL}",
+            json=dict(dto),
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 201
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 201
+    assert data['message'] == 'Comment created with successfully'
+    assert data['status'] == True
+
+    assert body['id'] is not None
+    assert isinstance(body['id'], int)
+    assert body['user_id'] == user_data.out.id
+    assert body['post_enterprise_id'] == post_enterprise_data.id
+
+    assert body['user']['id'] == user_data.out.id
+    assert body['post']['id'] == post_enterprise_data.id
+    assert body['parent_comment_id'] == comment_data.id
+
+@pytest.mark.asyncio
+async def test_create_comment():
+    user_data = await create_and_login_user()
+    category_data: Final = await create_category(user_data)
+    industry_data = await create_industry(user_data)
+    enterprise_data = await create_enterprise(user_data, industry_data)
+    post_enterprise_data: Final = await create_post_enterprise(user_data, enterprise_data, category_data)
+
+    dto = CreateCommentPostEnterpriseDTO(
+        content = ("abc" * 50),
+        post_enterprise_id = post_enterprise_data.id,
+        parent_comment_id = None
+    )
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.post(
+            f"{URL}",
+            json=dict(dto),
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 201
+
+    data = response.json()
+    body = response.json()['body']
+
+    assert data['code'] == 201
+    assert data['message'] == 'Comment created with successfully'
+    assert data['status'] == True
+
+    assert body['id'] is not None
+    assert isinstance(body['id'], int)
+    assert body['user_id'] == user_data.out.id
+    assert body['post_enterprise_id'] == post_enterprise_data.id
+
+    assert body['user']['id'] == user_data.out.id
+    assert body['post']['id'] == post_enterprise_data.id
+
+@pytest.mark.asyncio
+async def test_get_all_comment_create_comment():
+    user_data = await create_and_login_user()
+
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response: Final = await ac.get(
+            f"{URL}",
+            headers={"Authorization": f"Bearer {user_data.tokens.token}"}
+        )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data['items'] is not None
