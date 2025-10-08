ba73e10af4f2399fa4d04bca5d21504eb7210a32
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-08 13:40:00 -0300

    New methods

--- app/services/base/comment_post_user_service_base.py
+++ app/services/base/comment_post_user_service_base.py
@@ -1,5 +1,32 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from app.configs.db.database import CommentPostUserEntity
+from app.schemas.comment_post_user_schemas import CreateCommentPostUserDTO, UpdateCommentPostUserDTO
+from app.utils.filter.comment_post_user_filter import CommentPostUserFilter
 
 
 class CommentPostUserServiceBase(ABC):
 
+    @abstractmethod
+    async def get_by_id(self, id: int) -> CommentPostUserEntity | None:
+        pass
+
+    @abstractmethod
+    async def delete(self, comment: CommentPostUserEntity):
+        pass
+
+    @abstractmethod
+    async def get_all(self, filter: CommentPostUserFilter) -> list[CommentPostUserEntity]:
+        pass
+
+    @abstractmethod
+    async def exists_by_id(self, id: int) -> bool:
+        pass
+
+    @abstractmethod
+    async def create(self, user_id: int, dto: CreateCommentPostUserDTO) -> CommentPostUserEntity:
+        pass
+
+    @abstractmethod
+    async def update(self, comment: CommentPostUserEntity, dto: UpdateCommentPostUserDTO) -> CommentPostUserEntity:
+        pass
