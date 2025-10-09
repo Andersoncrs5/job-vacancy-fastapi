a51fa709b91920ac6b6b3a8d6843ab6c6c64a345
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-09 20:35:35 -0300

    Change name imports

--- app/schemas/favorite_comment_post_user_schemas.py
+++ app/schemas/favorite_comment_post_user_schemas.py
@@ -2,20 +2,20 @@ import uuid
 from datetime import datetime
 
 from app.configs.orjson.orjson_config import ORJSONModel
-from app.schemas.comment_post_enterprise_schemas import CommentPostEnterpriseOUT
+from app.schemas.comment_post_user_schemas import CommentPostUserOUT
 from app.schemas.user_schemas import UserOUT
 
 
-class FavoriteCommentPostEnterpriseOUT(ORJSONModel):
+class FavoriteCommentPostUserOUT(ORJSONModel):
     id: uuid.UUID
     user_id: int
-    comment_enterprise_id: int
+    comment_user_id: int
     created_at: datetime
     user: UserOUT
-    comment: CommentPostEnterpriseOUT
+    comment: CommentPostUserOUT
 
-class FavoriteCommentPostEnterpriseSimpleOUT(ORJSONModel):
+class FavoriteCommentPostUserSimpleOUT(ORJSONModel):
     id: uuid.UUID
     user_id: int
-    comment_enterprise_id: int
+    comment_user_id: int
     created_at: datetime
