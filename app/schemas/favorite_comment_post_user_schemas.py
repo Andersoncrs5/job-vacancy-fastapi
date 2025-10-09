0829b178a559d622b7329dc015c1644538dd8a0c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-09 14:45:21 -0300

    Impl schemas to FavoriteCommentPostEnterpriseEntity

---
+++ app/schemas/favorite_comment_post_user_schemas.py
@@ -0,0 +1,21 @@
+import uuid
+from datetime import datetime
+
+from app.configs.orjson.orjson_config import ORJSONModel
+from app.schemas.comment_post_enterprise_schemas import CommentPostEnterpriseOUT
+from app.schemas.user_schemas import UserOUT
+
+
+class FavoriteCommentPostUserOUT(ORJSONModel):
+    id: uuid.UUID
+    user_id: int
+    comment_enterprise_id: int
+    created_at: datetime
+    owner: UserOUT
+    comment: CommentPostEnterpriseOUT
+
+class FavoriteCommentPostUserSimpleOUT(ORJSONModel):
+    id: uuid.UUID
+    user_id: int
+    comment_enterprise_id: int
+    created_at: datetime
