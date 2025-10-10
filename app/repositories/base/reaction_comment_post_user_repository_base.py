a4a7d16f5c643bf896a41019ce01e3500d26ae81
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-09 21:16:23 -0300

    Impl ReactionCommentPostUserRepository Provider and Base

--- app/repositories/base/reaction_comment_post_user_repository_base.py
+++ app/repositories/base/reaction_comment_post_user_repository_base.py
@@ -1,9 +1,40 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+import uuid
+from abc import abstractmethod, ABC
 
-from app.repositories.providers.reaction_comment_post_user_repository_provider import \
-    ReactionCommentPostUserRepositoryBase
+from app.configs.db.database import ReactionCommentPostUserEntity
+from app.configs.db.enums import ReactionTypeEnum
 
 
-class ReactionCommentPostUserRepositoryProvider(ReactionCommentPostUserRepositoryBase):
-    def __init__(self, db: AsyncSession):
-        self.db = db
+class ReactionCommentPostUserRepositoryBase(ABC):
+    @abstractmethod
+    async def get_all(
+            self, user_id: int | None,
+            comment_user_id: int | None, reaction_type: ReactionTypeEnum | None
+    ) -> list[ReactionCommentPostUserEntity]:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: uuid.UUID) -> ReactionCommentPostUserEntity | None:
+        pass
+
+    @abstractmethod
+    async def get_by_user_id_and_comment_user_id(self, user_id: int,
+                                                 comment_user_id: int) -> ReactionCommentPostUserEntity | None:
+        pass
+
+    @abstractmethod
+    async def save(self, react: ReactionCommentPostUserEntity) -> ReactionCommentPostUserEntity:
+        pass
+
+    @abstractmethod
+    async def delete(self, react: ReactionCommentPostUserEntity):
+        pass
+
+    @abstractmethod
+    async def add(self, react: ReactionCommentPostUserEntity) -> ReactionCommentPostUserEntity:
+        pass
+
+    @abstractmethod
+    async def exists_by_user_id_and_comment_user_id(self, user_id: int,
+                                                    comment_user_id: int) -> bool:
+        pass
--- app/repositories/providers/reaction_comment_post_user_repository_provider.py
+++ app/repositories/providers/reaction_comment_post_user_repository_provider.py
@@ -1,4 +1,88 @@
-from abc import ABC
+from sqlalchemy import select, and_, func
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
+
+from app.configs.db.database import ReactionCommentPostUserEntity
+
+import uuid
+
+from app.configs.db.enums import ReactionTypeEnum
+from app.repositories.base.reaction_comment_post_user_repository_base import ReactionCommentPostUserRepositoryBase
 
 
-class ReactionCommentPostUserRepositoryBase(ABC):
+class ReactionCommentPostUserRepositoryProvider(ReactionCommentPostUserRepositoryBase):
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_all(
+            self, user_id: int | None,
+            comment_user_id: int | None,
+            reaction_type: ReactionTypeEnum | None
+    ) -> list[ReactionCommentPostUserEntity]:
+        stmt = select(ReactionCommentPostUserEntity)
+
+        if not user_id:
+            stmt = stmt.where(ReactionCommentPostUserEntity.user_id == user_id)
+
+        if not comment_user_id:
+            stmt = stmt.where(ReactionCommentPostUserEntity.comment_user_id == comment_user_id)
+
+        if not reaction_type:
+            stmt = stmt.where(ReactionCommentPostUserEntity.reaction_type == reaction_type)
+
+        result = await self.db.execute(stmt)
+        return list(result.scalars().all())
+
+    async def get_by_user_id_and_comment_user_id(
+            self, user_id: int, comment_user_id: int
+    ) -> ReactionCommentPostUserEntity | None:
+        stmt = select(ReactionCommentPostUserEntity).where(
+            and_(
+                ReactionCommentPostUserEntity.user_id == user_id,
+                ReactionCommentPostUserEntity.comment_user_id == comment_user_id,
+            )
+        )
+
+        result = await self.db.execute(stmt)
+
+        return result.scalars().first()
+
+    async def exists_by_user_id_and_comment_user_id(
+            self, user_id: int, comment_user_id: int
+    ) -> bool:
+        stmt = select(func.count(ReactionCommentPostUserEntity.id)).where(
+            and_(
+                ReactionCommentPostUserEntity.user_id == user_id,
+                ReactionCommentPostUserEntity.comment_user_id == comment_user_id,
+            )
+        )
+
+        result = await self.db.scalar(stmt)
+
+        return bool(result and result > 0)
+
+    async def delete(self, react: ReactionCommentPostUserEntity):
+        await self.db.delete(react)
+        await self.db.commit()
+
+    async def add(self, react: ReactionCommentPostUserEntity) -> ReactionCommentPostUserEntity:
+        self.db.add(react)
+        await self.db.commit()
+        await self.db.refresh(react)
+
+        return react
+
+    async def get_by_id(self, id: uuid.UUID) -> ReactionCommentPostUserEntity | None:
+        stmt = select(ReactionCommentPostUserEntity).where(
+            ReactionCommentPostUserEntity.id == id
+        )
+
+        result = await self.db.execute(stmt)
+
+        return result.scalars().first()
+
+    async def save(self, react: ReactionCommentPostUserEntity) -> ReactionCommentPostUserEntity:
+        await self.db.commit()
+        await self.db.refresh(react)
+
+        return react
