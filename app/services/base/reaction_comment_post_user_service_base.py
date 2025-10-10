5d45e1def78ac28147e10812dc0b79641d4172c4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-10 16:29:07 -0300

    Set methods get_by_user_id_and_comment_user_id, exists_by_user_id_and_comment_user_id, create, delete and toggle_reaction_type

--- app/services/base/reaction_comment_post_user_service_base.py
+++ app/services/base/reaction_comment_post_user_service_base.py
@@ -1,4 +1,31 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from app.configs.db.database import ReactionCommentPostUserEntity
+from app.schemas.reaction_comment_post_user_schemas import CreateReactionCommentPostUserDTO
 
 
 class ReactionCommentPostUserServiceBase(ABC):
+
+    @abstractmethod
+    async def get_by_user_id_and_comment_user_id(
+            self, user_id: int, comment_user_id: int
+    ) -> ReactionCommentPostUserEntity | None:
+        pass
+
+    @abstractmethod
+    async def exists_by_user_id_and_comment_user_id(
+            self, user_id: int, comment_user_id: int
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    async def create(self, user_id: int, dto: CreateReactionCommentPostUserDTO) -> ReactionCommentPostUserEntity:
+        pass
+
+    @abstractmethod
+    async def delete(self, react: ReactionCommentPostUserEntity):
+        pass
+
+    @abstractmethod
+    async def toggle_reaction_type(self, react :ReactionCommentPostUserEntity) -> ReactionCommentPostUserEntity:
+        pass
