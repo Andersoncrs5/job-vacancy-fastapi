57fe404396179178fd0f19b1f09cc73fdca28a55
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-11 15:07:13 -0300

    New enum ColumnUserMetricEnum

--- app/utils/enums/sum_red.py
+++ app/utils/enums/sum_red.py
@@ -1,5 +1,22 @@
 from enum import Enum
 
-class SumRedEnum(Enum):
+class SumRedEnum(str, Enum):
     SUM = "SUM"
     RED = "RED"
+
+class ColumnUserMetricEnum(str, Enum):
+    post_count = "post_count"
+    favorite_post_count = "favorite_post_count"
+    comment_count = "comment_count"
+    favorite_comment_count = "favorite_comment_count"
+    follower_count = "follower_count"
+    followed_count = "followed_count"
+    share_count = "share_count"
+    connection_count = "connection_count"
+    blocked_count = "blocked_count"
+    reaction_comment_given_count = "reaction_comment_given_count"
+    reaction_comment_received_count = "reaction_comment_received_count"
+    enterprise_follow_count = "enterprise_follow_count"
+    enterprise_follower_count = "enterprise_follower_count"
+    profile_view_count = "profile_view_count"
+    vacancy_application_count = "vacancy_application_count"
