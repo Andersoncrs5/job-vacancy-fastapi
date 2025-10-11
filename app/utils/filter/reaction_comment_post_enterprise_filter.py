42462b9a4a637a160b4ce3d8885665d22588ffe9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-10 21:02:44 -0300

    Change type all param

--- app/utils/filter/reaction_comment_post_enterprise_filter.py
+++ app/utils/filter/reaction_comment_post_enterprise_filter.py
@@ -1,13 +1,16 @@
+from typing import Optional
+
 from fastapi_filter.contrib.sqlalchemy import Filter
+from pydantic import Field
 
 from app.configs.db.database import ReactionCommentPostEnterpriseEntity
 from app.configs.db.enums import ReactionTypeEnum
 
 
 class ReactionCommentPostEnterpriseFilter(Filter):
-    user_id: int | None
-    comment_enterprise_id: int | None
-    reaction_type: ReactionTypeEnum | None
+    user_id: Optional[int] = Field(None)
+    comment_enterprise_id: Optional[int] = Field(None)
+    reaction_type: Optional[ReactionTypeEnum] = Field(None)
 
     class Constants(Filter.Constants):
         model = ReactionCommentPostEnterpriseEntity
