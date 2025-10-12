4a1fc5443b331121ffc26752b989102cc53ac393
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-12 11:50:36 -0300

    New param ENTERPRISE_METRIC to EntityEnum

--- app/schemas/event_message_schemas.py
+++ app/schemas/event_message_schemas.py
@@ -9,6 +9,7 @@ from enum import Enum
 class EntityEnum(str, Enum):
     USER_METRIC = "USER_METRIC"
     VACANCY_METRIC = "VACANCY_METRIC"
+    ENTERPRISE_METRIC = "ENTERPRISE_METRIC"
 
 class EventMessageMetric(ORJSONModel):
     event_id: str
