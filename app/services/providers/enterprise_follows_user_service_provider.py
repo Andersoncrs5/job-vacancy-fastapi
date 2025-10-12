a9d991be8623bc25532be69490b674769013fbae
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-11 21:22:28 -0300

    Impled enterprise_follows_user service base and provider

---
+++ app/services/providers/enterprise_follows_user_service_provider.py
@@ -0,0 +1,34 @@
+from app.configs.db.database import EnterpriseFollowsUserEntity
+from app.repositories.providers.enterprise_follows_user_repository_provider import \
+    EnterpriseFollowsUserRepositoryProvider
+from app.services.base.enterprise_follows_user_service_base import EnterpriseFollowsUserServiceBase
+
+
+class EnterpriseFollowsUserServiceProvider(EnterpriseFollowsUserServiceBase):
+    def __init__(self, repository: EnterpriseFollowsUserRepositoryProvider):
+        self.repository = repository
+
+    async def create(self, enterprise_id: int, user_id: int) -> EnterpriseFollowsUserEntity:
+        follow = EnterpriseFollowsUserEntity(
+            enterprise_id=enterprise_id,
+            user_id=user_id,
+        )
+
+        return await self.repository.add(follow)
+
+    async def delete(self, follow: EnterpriseFollowsUserEntity):
+        await self.repository.delete(follow)
+
+    async def get_by_enterprise_id_and_user_id(
+            self,
+            enterprise_id: int,
+            user_id: int
+    ) -> EnterpriseFollowsUserEntity | None:
+        return await self.repository.get_by_enterprise_id_and_user_id(enterprise_id=enterprise_id, user_id=user_id)
+
+    async def exists_by_enterprise_id_and_user_id(
+            self,
+            enterprise_id: int,
+            user_id: int
+    ) -> bool:
+        return await self.repository.exists_by_enterprise_id_and_user_id(enterprise_id=enterprise_id, user_id=user_id)
