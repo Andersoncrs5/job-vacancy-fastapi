544102aab99f919fb66493c66ef02aae3067ab77
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-13 21:53:18 -0300

    Remake method setup_logging

--- app/configs/logs/log_config.py
+++ app/configs/logs/log_config.py
@@ -1,5 +1,10 @@
 import structlog
 import logging
+from structlog.processors import TimeStamper, KeyValueRenderer
+from structlog.stdlib import LoggerFactory, add_log_level
+from structlog.contextvars import merge_contextvars
+from structlog.typing import BindableLogger
+
 
 def setup_logging():
     logging.basicConfig(
@@ -9,12 +14,16 @@ def setup_logging():
 
     structlog.configure(
         processors=[
-            structlog.contextvars.merge_contextvars,
-            structlog.processors.TimeStamper(fmt="iso"),
-            structlog.processors.JSONRenderer(),
+            merge_contextvars,
+            add_log_level,
+            TimeStamper(fmt="iso", utc=True),
+            KeyValueRenderer(
+                key_order=['timestamp', 'level', 'event'],
+                sort_keys=False
+            ),
         ],
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        wrapper_class=structlog.stdlib.BoundLogger,
+        logger_factory=LoggerFactory(),
+        #wrapper_class=BindableLogger,
         cache_logger_on_first_use=True,
     )
 
