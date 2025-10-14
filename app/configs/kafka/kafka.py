54d7df20d6576ea140b1d9d9a826143fa9a1aa50
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-13 21:52:55 -0300

    Removed kafka.py

--- app/configs/kafka/kafka.py
+++
@@ -1,7 +0,0 @@
-from typing import Final
-from aiokafka import AIOKafkaProducer
-
-KAFKA_BROKER: Final[str] = "localhost:29092"
-SUM_RED_METRIC_TOPIC: Final[str] = "sum_red_metric_topic"
-
-producer: AIOKafkaProducer | None = None
