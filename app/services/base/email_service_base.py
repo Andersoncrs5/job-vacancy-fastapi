8fe7060a3bf124ef08751e33756f877b23cf7e64
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-14 16:22:04 -0300

    New methods send_email_depend_application_statusm,send_email_bye, send_email_informing_application

--- app/services/base/email_service_base.py
+++ app/services/base/email_service_base.py
@@ -1,9 +1,23 @@
 from abc import ABC, abstractmethod
 
+from app.configs.db.enums import ApplicationStatusEnum
 from app.configs.kafka_configs.events.event_message_email import EventMessageEmail
 
 
 class EmailServiceBase(ABC):
+
+    @abstractmethod
+    async def send_email_depend_application_status(self, user_email: str, status: ApplicationStatusEnum,
+                                                   data: dict):
+        pass
+
+    @abstractmethod
+    async def send_email_bye(self, user_email: str, subject: str, data: dict):
+        pass
+
+    @abstractmethod
+    async def send_email_informing_application(self, user_email: str, subject: str, data: dict):
+        pass
 
     @abstractmethod
     async def send_email_welcome_user(self, user_email: str, subject: str, data: dict):
