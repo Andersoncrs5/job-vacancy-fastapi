6f2aec5c146e94f4888955d34155648c2a0c1646
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-15 10:39:19 -0300

    New schemas CommentPostUserMetricOUT

---
+++ app/schemas/comment_post_user_metric_schemas.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from app.configs.orjson.orjson_config import ORJSONModel
+
+
+class CommentPostUserMetricOUT(ORJSONModel):
+    comment_id: int
+    replies_count: int
+    edited_count: int
+    views_count: int
+    shares_count: int
+    reactions_like_count: int
+    reactions_dislike_count: int
+    favorites_count: int
+    created_at: datetime
+    updated_at: datetime
