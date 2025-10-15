5658d22e356f682de48e13cf592d39db53daf4be
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-15 19:34:26 -0300

    Improvement in validation with pydantic

--- app/schemas/category_schemas.py
+++ app/schemas/category_schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from app.configs.orjson.orjson_config import ORJSONModel
 
@@ -18,27 +18,22 @@ class CategoryOUT(ORJSONModel):
     updated_at: datetime | str        
 
 class CreateCategoryDTO(ORJSONModel):
-    name: str
-    slug: str
-    description: str | None
-    order: int
-    icon_url: str | None
+    name: str = Field(..., min_length=3, max_length=100, description="The category name must be between 3 and 100 characters.")
+    slug: str = Field(..., min_length=3, max_length=220, description="The slug must be between 3 and 220 characters and unique.")
+    description: str | None = Field(None, max_length=1000, description="The category description (max 1000 characters).")
+    order: int = Field(0, ge=0, description="The category order (must be greater than or equal to 0).")
+    icon_url: str | None = Field(None, max_length=500, description="URL of the category icon (max 500 characters).")
+    parent_id: int | None = Field(None, description="ID of the parent category for subcategories.")
 
     def to_category_entity(self):
         from app.configs.db.database import CategoryEntity
-
-        return CategoryEntity(
-            name = self.name,
-            slug = self.slug,
-            description = self.description,
-            order = self.order,
-            icon_url = self.icon_url,
-        )
-
+        return CategoryEntity(**self.model_dump(exclude_none=True))
 
 class UpdateCategoryDTO(ORJSONModel):
-    name: str | None
-    slug: str | None
-    description: str | None
-    order: int | None
-    icon_url: str | None
+    name: str | None = Field(None, min_length=3, max_length=100, description="The category name must be between 3 and 100 characters.")
+    slug: str | None = Field(None, min_length=3, max_length=220, description="The slug must be between 3 and 220 characters and unique.")
+    description: str | None = Field(None, max_length=1000, description="The category description (max 1000 characters).")
+    is_active: bool | None = Field(None, description="Sets whether the category is active.")
+    order: int | None = Field(None, ge=0, description="The category order (must be greater than or equal to 0).")
+    icon_url: str | None = Field(None, max_length=500, description="URL of the category icon (max 500 characters).")
+    parent_id: int | None = Field(None, description="ID of the parent category for subcategories.")
