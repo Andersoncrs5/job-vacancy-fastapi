c7fdc51484d8fe8a4788df34cb5c2ff3e27dd36a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-15 19:34:16 -0300

    Improvement in validation with pydantic

--- app/schemas/area_schemas.py
+++ app/schemas/area_schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime, date
 from app.configs.orjson.orjson_config import ORJSONModel
 
@@ -12,21 +12,16 @@ class AreaOUT(ORJSONModel):
     updated_at: datetime | str
 
 class CreateAreaDTO(ORJSONModel):
-    name: str
-    description: str | None
-    is_active: bool
+    name: str = Field(..., min_length=4, max_length=50, description="Name should is between range of 4 and 50")
+    description: str | None = Field(None, min_length=4, max_length=400, description="description should be under of 400")
+    is_active: bool = Field(True)
 
     def to_entity(self):
         from app.configs.db.database import AreaEntity
-
-        return AreaEntity(
-            name = self.name,
-            description = self.description,
-            is_active = self.is_active,
-        )
+        return AreaEntity(**self.model_dump())
 
 class UpdateAreaDTO(ORJSONModel):
-    name: str | None
-    description: str | None
-    is_active: bool | None
+    name: str | None = Field(None, min_length=4, max_length=50, description="Name should is between range of 4 and 50")
+    description: str | None = Field(None, min_length=4, max_length=400, description="description should be under of 400")
+    is_active: bool | None = Field(None)
     
