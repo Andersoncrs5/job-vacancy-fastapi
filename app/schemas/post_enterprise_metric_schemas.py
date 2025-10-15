879447fd8cde6c79a680330b9d474e24457ea3dc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-15 10:39:38 -0300

    New schemas PostEnterpriseMetricOUT

---
+++ app/schemas/post_enterprise_metric_schemas.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from app.configs.orjson.orjson_config import ORJSONModel
+
+
+class PostEnterpriseMetricOUT(ORJSONModel):
+    post_id: int
+    views_count: int
+    shares_count: int
+    reactions_like_count: int
+    reactions_dislike_count: int
+    favorites_count: int
+    comments_count: int
+    created_at: datetime
+    updated_at: datetime
