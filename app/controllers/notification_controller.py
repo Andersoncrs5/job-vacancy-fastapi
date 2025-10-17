a3433bd10e59ddd039079e2d77bdd79f21cc78b6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-16 21:26:21 -0300

    New endpoint toggle_is_view_end

--- app/controllers/notification_controller.py
+++ app/controllers/notification_controller.py
@@ -27,6 +27,86 @@ router: Final[APIRouter] = APIRouter(
 bearer_scheme: Final[HTTPBearer] = HTTPBearer()
 
 @router.get(
+    "/{noti_id}/status/is-view",
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBody[NotificationOUT],
+    responses={
+        status.HTTP_404_NOT_FOUND: RESPONSE_404,
+        status.HTTP_400_BAD_REQUEST: RESPONSE_400,
+    }
+)
+async def toggle_is_view_end(
+    noti_id: int,
+    notification_service: NotificationServiceProvider = Depends(get_notification_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if noti_id <= 0:
+        return ORJSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=dict(ResponseBody(
+                code=status.HTTP_400_BAD_REQUEST,
+                message="id is required",
+                status=False,
+                body=None,
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int] = jwt_service.extract_user_id_v2(token)
+
+        noti: Final = await notification_service.get_by_id(noti_id)
+        if noti is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody(
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Notification not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        noti_updated: Final = await notification_service.toggle_is_view(noti)
+
+        out = noti_updated.to_out()
+
+        return ORJSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[dict](
+                message="Notification status updated with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=dict(out),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.get(
     '',
     status_code=status.HTTP_200_OK,
     response_model=Page[NotificationOUT],
