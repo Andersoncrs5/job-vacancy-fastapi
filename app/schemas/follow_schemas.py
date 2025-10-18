c3d1ac3820e9b3596d016ee7eea8a97c80a3c2ec
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-17 22:39:04 -0300

    New params to FollowOUT and new class UpdateFollowDTO

--- app/schemas/follow_schemas.py
+++ app/schemas/follow_schemas.py
@@ -7,6 +7,12 @@ from app.schemas.user_schemas import UserOUT
 class FollowOUT(ORJSONModel):
     follower_id: int
     followed_id: int
+    receive_post: bool
+    receive_comment: bool
     created_at: datetime
     follower: UserOUT
     followed: UserOUT
+
+class UpdateFollowDTO(ORJSONModel):
+    receive_post: bool | None
+    receive_comment: bool | None
