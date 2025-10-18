1f6a934f933e0940fc07ed556854da26268b321c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-17 22:39:19 -0300

    More params to NotificationFilter

--- app/utils/filter/notification_filter.py
+++ app/utils/filter/notification_filter.py
@@ -3,8 +3,13 @@ from pydantic import Field
 from typing import Optional
 from datetime import datetime
 from app.configs.db.database import NotificationEntity
+from app.configs.db.enums import NotificationTypeEnum
+
 
 class NotificationFilter(Filter):
+    user_id: Optional[int] = Field(None, description="Filter by recipient user ID.")
+    entity_id: Optional[int] = Field(None, description="Filter by related entity ID (e.g., Post ID, Comment ID).")
+    type: Optional[NotificationTypeEnum] = Field(None, description="Filter by notification type.")
     created_at__gte: Optional[datetime] = Field(None, description="Filter by creation date, greater than or equal to.")
     created_at__lte: Optional[datetime] = Field(None, description="Filter by creation date, less than or equal to.")
 
