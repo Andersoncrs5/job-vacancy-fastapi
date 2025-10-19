20857f01a50e892ebeb287490c0b61756b7c6513
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-19 16:08:29 -0300

    Impled GenericService

--- app/services/base/comment_post_user_service_base.py
+++ app/services/base/comment_post_user_service_base.py
@@ -1,27 +1,15 @@
 from abc import ABC, abstractmethod
+from typing import TypeVar
 
 from app.configs.db.database import CommentPostUserEntity
 from app.schemas.comment_post_user_schemas import CreateCommentPostUserDTO, UpdateCommentPostUserDTO
 from app.utils.filter.comment_post_user_filter import CommentPostUserFilter
 
+CommentEntity = TypeVar('CommentEntity')
+CommentRepo = TypeVar('CommentRepo')
+CommentFilter = TypeVar('CommentFilter')
 
 class CommentPostUserServiceBase(ABC):
-
-    @abstractmethod
-    async def get_by_id(self, id: int) -> CommentPostUserEntity | None:
-        pass
-
-    @abstractmethod
-    async def delete(self, comment: CommentPostUserEntity):
-        pass
-
-    @abstractmethod
-    async def get_all(self, filter: CommentPostUserFilter) -> list[CommentPostUserEntity]:
-        pass
-
-    @abstractmethod
-    async def exists_by_id(self, id: int) -> bool:
-        pass
 
     @abstractmethod
     async def create(self, user_id: int, dto: CreateCommentPostUserDTO) -> CommentPostUserEntity:
--- app/services/providers/comment_post_user_service_provider.py
+++ app/services/providers/comment_post_user_service_provider.py
@@ -4,24 +4,20 @@ from app.configs.db.database import CommentPostUserEntity
 from app.repositories.providers.comment_post_user_repository_provider import CommentPostUserRepositoryProvider
 from app.schemas.comment_post_user_schemas import CreateCommentPostUserDTO, UpdateCommentPostUserDTO
 from app.services.base.comment_post_user_service_base import CommentPostUserServiceBase
+from app.services.generics.generic_service import GenericService
 from app.utils.filter.comment_post_user_filter import CommentPostUserFilter
 
 
-class CommentPostUserServiceProvider(CommentPostUserServiceBase):
+class CommentPostUserServiceProvider(
+    CommentPostUserServiceBase,
+    GenericService[
+        CommentPostUserEntity,
+        CommentPostUserRepositoryProvider,
+        CommentPostUserFilter
+    ]
+):
     def __init__(self, repository: CommentPostUserRepositoryProvider):
-        self.repository = repository
-
-    async def get_by_id(self, id: int) -> CommentPostUserEntity | None:
-        return await self.repository.get_by_id(id)
-
-    async def delete(self, comment: CommentPostUserEntity):
-        await self.repository.delete(comment)
-
-    async def get_all(self, filter: CommentPostUserFilter) -> list[CommentPostUserEntity]:
-        return await self.repository.get_all(filter)
-
-    async def exists_by_id(self, id: int) -> bool:
-        return await self.repository.exists_by_id(id)
+        super().__init__(repository)
 
     async def create(self, user_id: int, dto: CreateCommentPostUserDTO) -> CommentPostUserEntity:
         comment = dto.to_entity()
