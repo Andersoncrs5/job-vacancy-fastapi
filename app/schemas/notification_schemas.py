bd8119e50e7adeeb8f0bc84771e4c3b57dfc257a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-20 14:44:36 -0300

    Extends NotificationBaseOUT

--- app/schemas/notification_schemas.py
+++ app/schemas/notification_schemas.py
@@ -2,16 +2,8 @@ from datetime import datetime
 
 from app.configs.db.enums import NotificationTypeEnum
 from app.configs.orjson.orjson_config import ORJSONModel
+from app.schemas.base import NotificationBaseOUT
 
 
-class NotificationOUT(ORJSONModel):
-    id: int
+class NotificationOUT(NotificationBaseOUT):
     user_id: int
-    title: str
-    content: str
-    link: str | None
-    is_view: bool
-    type: NotificationTypeEnum
-    entity_id: int | None
-    created_at: datetime
-    updated_at: datetime
