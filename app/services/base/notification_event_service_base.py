ba986f5d7202bbfbc31f5acc2b6e0615f1a0e743
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-21 21:58:59 -0300

    Improvement in style

--- app/services/base/notification_event_service_base.py
+++ app/services/base/notification_event_service_base.py
@@ -8,7 +8,7 @@ class NotificationEventServiceBase(ABC):
     @abstractmethod
     async def get_users_name_mentioned_send_an_event_to_kafka(self, content: str, entity_id: int):
         pass
-    
+
     @abstractmethod
     async def notify_event_to_kafka(self, entity_id: int, actor_id: int | None, _type: NotificationTypeEnum,
                                     data: dict):
