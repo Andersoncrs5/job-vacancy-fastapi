ac5de5fb5a136e9b1c707b05cdfb74be276a7c20
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-22 15:47:03 -0300

    impled notification_enterprise_controller.py

---
+++ app/controllers/notification_enterprise_controller.py
@@ -0,0 +1,299 @@
+from datetime import datetime
+
+from fastapi import APIRouter, status
+from fastapi.responses import ORJSONResponse
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi_pagination import Page, add_pagination, paginate
+
+from app.configs.db.database import NotificationEnterpriseEntity
+from app.dependencies.service_dependency import *
+from app.schemas.notification_enterprise_schemas import NotificationEnterpriseOUT
+from app.schemas.notification_schemas import NotificationOUT
+from app.services.base.jwt_service_base import JwtServiceBase
+from app.utils.filter.notification_enterprise_filter import NotificationEnterpriseFilter
+from app.utils.res.responses_http import *
+
+URL = "/api/v1/notification-enterprise"
+
+router: Final[APIRouter] = APIRouter(
+    prefix=URL,
+    tags=["Notifications Enterprise"],
+    responses={
+        status.HTTP_500_INTERNAL_SERVER_ERROR: RESPONSE_500,
+        status.HTTP_401_UNAUTHORIZED: RESPONSE_401,
+    },
+    deprecated=False,
+)
+
+bearer_scheme: Final[HTTPBearer] = HTTPBearer()
+
+@router.get(
+    "/{noti_id}/status/is-view",
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBody[NotificationOUT],
+    responses={
+        status.HTTP_404_NOT_FOUND: RESPONSE_404,
+        status.HTTP_400_BAD_REQUEST: RESPONSE_400,
+    }
+)
+async def toggle_is_view_end(
+    noti_id: int,
+    notification_service: NotificationEnterpriseServiceProvider = Depends(get_notification_enterprise_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if noti_id <= 0:
+        return ORJSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=dict(ResponseBody(
+                code=status.HTTP_400_BAD_REQUEST,
+                message="id is required",
+                status=False,
+                body=None,
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int] = jwt_service.extract_user_id_v2(token)
+
+        noti: Final = await notification_service.get_by_id(noti_id)
+        if noti is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody(
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Notification not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        noti_updated: Final = await notification_service.toggle_is_view(noti)
+
+        out = noti_updated.to_out()
+
+        return ORJSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[dict](
+                message="Notification status updated with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=dict(out),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.get(
+    '',
+    status_code=status.HTTP_200_OK,
+    response_model=Page[NotificationEnterpriseOUT],
+)
+async def get_all(
+    filter: NotificationEnterpriseFilter = Depends(),
+    notification_service: NotificationEnterpriseServiceProvider = Depends(get_notification_enterprise_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int] = jwt_service.extract_user_id_v2(token)
+
+        _all: Final[list[NotificationEnterpriseEntity]] = await notification_service.get_all(filter)
+
+        return paginate(_all)
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.get(
+    "/{noti_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBody[NotificationOUT],
+    responses={
+        status.HTTP_404_NOT_FOUND: RESPONSE_404,
+        status.HTTP_400_BAD_REQUEST: RESPONSE_400,
+    }
+)
+async def get(
+    noti_id: int,
+    notification_service: NotificationEnterpriseServiceProvider = Depends(get_notification_enterprise_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if noti_id <= 0:
+        return ORJSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=dict(ResponseBody(
+                code=status.HTTP_400_BAD_REQUEST,
+                message="id is required",
+                status=False,
+                body=None,
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int] = jwt_service.extract_user_id_v2(token)
+
+        noti: Final = await notification_service.get_by_id(noti_id)
+        if noti is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody(
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Notification not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        out = noti.to_out()
+
+        return ORJSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody[dict](
+                message="Notification found with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=dict(out),
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+@router.delete(
+    "/{noti_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBody,
+    responses={
+        status.HTTP_404_NOT_FOUND: RESPONSE_404,
+        status.HTTP_400_BAD_REQUEST: RESPONSE_400,
+    }
+)
+async def delete(
+    noti_id: int,
+    notification_service: NotificationEnterpriseServiceProvider = Depends(get_notification_enterprise_provider_dependency),
+    jwt_service: JwtServiceBase = Depends(get_jwt_service_dependency),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+):
+    if noti_id <= 0:
+        return ORJSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=dict(ResponseBody(
+                code=status.HTTP_400_BAD_REQUEST,
+                message="id is required",
+                status=False,
+                body=None,
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    try:
+        token: Final[str] = jwt_service.valid_credentials(credentials)
+
+        user_id: Final[int] = jwt_service.extract_user_id_v2(token)
+
+        noti: Final = await notification_service.get_by_id(noti_id)
+        if noti is None:
+            return ORJSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=dict(ResponseBody(
+                    code=status.HTTP_404_NOT_FOUND,
+                    message="Notification not found",
+                    status=False,
+                    body=None,
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+        await notification_service.delete(noti)
+
+        return ORJSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=dict(ResponseBody(
+                message="Notification deleted with successfully",
+                code=status.HTTP_200_OK,
+                status=True,
+                body=None,
+                timestamp=str(datetime.now()),
+                version = 1,
+                path = None
+            ))
+        )
+
+    except Exception as e:
+        return ORJSONResponse(
+                status_code=500,
+                content=dict(ResponseBody[Any](
+                    code=500,
+                    message="Error in server! Please try again later",
+                    status=False,
+                    body=str(e),
+                    timestamp=str(datetime.now()),
+                    version = 1,
+                    path = None
+                ))
+            )
+
+add_pagination(router)
