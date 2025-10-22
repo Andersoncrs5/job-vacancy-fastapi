652aa126518cea94d65d01357ec26d0e24161fa1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-22 15:48:29 -0300

    New roles_repository base and provider

--- app/repositories/base/roles_repository_base.py
+++ app/repositories/base/roles_repository_base.py
@@ -5,7 +5,15 @@ from app.configs.db.database import RolesEntity
 
 class RolesRepositoryBase(ABC):
     @abstractmethod
+    async def exists_by_slug(self, slug: str) -> bool:
+        pass
+
+    @abstractmethod
     async def get_by_slug(self, slug: str) -> RolesEntity | None:
+        pass
+
+    @abstractmethod
+    async def get_by_title(self, title: str) -> RolesEntity | None:
         pass
 
     @abstractmethod
--- app/repositories/providers/roles_repository_provider.py
+++ app/repositories/providers/roles_repository_provider.py
@@ -6,13 +6,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.configs.db.database import RolesEntity
 from app.repositories.base.roles_repository_base import RolesRepositoryBase
 from app.repositories.generics.generic_repository import GenericRepository
+from app.utils.filter.roles_filter import RolesFilter
 
 
 class RolesRepositoryProvider(
     RolesRepositoryBase,
     GenericRepository[
         RolesEntity,
-        None,
+        RolesFilter,
         int,
         RolesEntity,
     ]
@@ -29,8 +30,24 @@ class RolesRepositoryProvider(
 
         return bool(result and result > 0)
 
+    async def exists_by_slug(self, slug: str) -> bool:
+        stmt = select(func.count(RolesEntity.id)).where(
+            RolesEntity.slug == slug
+        )
+
+        result = await self.db.scalar(stmt)
+
+        return bool(result and result > 0)
+
     async def get_by_slug(self, slug: str) -> RolesEntity | None:
         stmt = select(RolesEntity).where(RolesEntity.slug == slug)
+
+        result = await self.db.execute(stmt)
+
+        return result.scalars().first()
+
+    async def get_by_title(self, title: str) -> RolesEntity | None:
+        stmt = select(RolesEntity).where(RolesEntity.title == title)
 
         result = await self.db.execute(stmt)
 
