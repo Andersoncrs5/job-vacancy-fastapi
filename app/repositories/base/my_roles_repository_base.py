0751a871a84f94391cf67d8f641942f3a1e83c1d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-22 15:46:36 -0300

    Impled my_roles_repository base and provider

--- app/repositories/base/my_roles_repository_base.py
+++ app/repositories/base/my_roles_repository_base.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 
-from app.configs.db.database import MyRolesEntity
+from app.configs.db.database import UserRolesEntity
 
 
 class MyRolesRepositoryBase(ABC):
     @abstractmethod
-    async def get_by_user_id_and_role_id(self, user_id: int, role_id: int) -> (MyRolesEntity | None):
+    async def get_by_user_id_and_role_id(self, user_id: int, role_id: int) -> (UserRolesEntity | None):
         pass
 
     @abstractmethod
--- app/repositories/providers/my_roles_repository_provider.py
+++ app/repositories/providers/my_roles_repository_provider.py
@@ -3,7 +3,7 @@ from typing import Final
 from sqlalchemy import select, func, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.configs.db.database import MyRolesEntity
+from app.configs.db.database import UserRolesEntity
 from app.repositories.base.my_roles_repository_base import MyRolesRepositoryBase
 from app.repositories.generics.generic_repository import GenericRepository
 from app.utils.filter.my_role_filter import MyRolesFilter
@@ -12,20 +12,20 @@ from app.utils.filter.my_role_filter import MyRolesFilter
 class MyRolesRepositoryProvider(
     MyRolesRepositoryBase,
     GenericRepository[
-        MyRolesEntity,
+        UserRolesEntity,
         MyRolesFilter,
         int,
-        MyRolesEntity,
+        UserRolesEntity,
     ]
 ):
     def __init__(self, db: AsyncSession):
-        super().__init__(db=db, entity_class=MyRolesEntity)
+        super().__init__(db=db, entity_class=UserRolesEntity)
 
     async def exists_by_user_id_and_role_id(self, user_id: int, role_id: int) -> bool:
-        stmt = select(func.count(MyRolesEntity.id)).where(
+        stmt = select(func.count(UserRolesEntity.id)).where(
             and_(
-                MyRolesEntity.role_id == role_id,
-                MyRolesEntity.user_id == user_id
+                UserRolesEntity.role_id == role_id,
+                UserRolesEntity.user_id == user_id
             )
         )
 
@@ -33,11 +33,11 @@ class MyRolesRepositoryProvider(
 
         return bool(result and result > 0)
 
-    async def get_by_user_id_and_role_id(self, user_id: int, role_id: int) -> (MyRolesEntity | None):
-        stmt = select(MyRolesEntity).where(
+    async def get_by_user_id_and_role_id(self, user_id: int, role_id: int) -> (UserRolesEntity | None):
+        stmt = select(UserRolesEntity).where(
             and_(
-                MyRolesEntity.role_id == role_id,
-                MyRolesEntity.user_id == user_id
+                UserRolesEntity.role_id == role_id,
+                UserRolesEntity.user_id == user_id
             )
         )
 
