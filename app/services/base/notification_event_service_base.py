5e74006eb33f9b9d7965b6aeefc1725eda19d994
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-21 21:57:25 -0300

    New method get_users_name_mentioned_send_an_event_to_kafka

--- app/services/base/notification_event_service_base.py
+++ app/services/base/notification_event_service_base.py
@@ -6,6 +6,10 @@ from app.configs.db.enums import NotificationTypeEnum
 class NotificationEventServiceBase(ABC):
 
     @abstractmethod
+    async def get_users_name_mentioned_send_an_event_to_kafka(self, content: str, entity_id: int):
+        pass
+    
+    @abstractmethod
     async def notify_event_to_kafka(self, entity_id: int, actor_id: int | None, _type: NotificationTypeEnum,
                                     data: dict):
         pass
--- app/services/providers/notification_event_service_provider.py
+++ app/services/providers/notification_event_service_provider.py
@@ -1,9 +1,11 @@
+import re
 from datetime import datetime
 from uuid import uuid4
 
 from aiokafka import AIOKafkaProducer
 
 from app.configs.db.enums import NotificationTypeEnum
+from app.configs.kafka_configs.events.event_mention import EventMention
 from app.configs.kafka_configs.events.event_notification import EventNotification
 from app.configs.kafka_configs.kafka_admin import NOTIFICATION_TOPIC
 from app.services.base.notification_event_service_base import NotificationEventServiceBase
@@ -13,6 +15,22 @@ from app.services.kafka_service import send_message_to_kafka
 class NotificationEventServiceProvider(NotificationEventServiceBase):
     def __init__(self, producer: AIOKafkaProducer):
         self.producer = producer
+
+    async def get_users_name_mentioned_send_an_event_to_kafka(self, content: str, entity_id: int):
+        pattern = r'@(\S+)'
+        names = re.findall(pattern, content)
+
+        event = EventMention(
+            entity_id=entity_id,
+            event_id=uuid4(),
+            created_at=datetime.now(),
+            source_service="NotificationEventServiceProvider | get_users_name_mentioned_send_an_event_to_kafka",
+            data={},
+            metadata={},
+            names=names
+        )
+
+        await send_message_to_kafka(self.producer, event.model_dump_json(), NOTIFICATION_TOPIC)
 
     async def notify_user_about(self, entity_id: int, actor_id: int | None, _type: NotificationTypeEnum, data: dict):
         event = EventNotification(
