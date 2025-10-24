0391d0c5b3d852088820de8b369a5972dcb304cb
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-23 21:03:02 -0300

    Impled BaseSchemas

--- app/schemas/roles_schemas.py
+++ app/schemas/roles_schemas.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 
 from app.configs.orjson.orjson_config import ORJSONModel
+from app.schemas.base import BaseSchemas
 
 
-class RoleOUT(ORJSONModel):
+class RoleOUT(BaseSchemas):
     id: int
     title: str
     slug: str
     is_active: bool
     is_immutable: bool
-    created_at: datetime
-    updated_at: datetime
