fd46d256369366bac181769dfb0bb13a546c8c3a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-23 21:01:26 -0300

    New base to schemas

--- app/schemas/base.py
+++ app/schemas/base.py
@@ -3,8 +3,11 @@ from datetime import datetime
 from app.configs.db.enums import NotificationTypeEnum
 from app.configs.orjson.orjson_config import ORJSONModel
 
+class BaseSchemas(ORJSONModel):
+    created_at: datetime
+    updated_at: datetime
 
-class NotificationBaseOUT(ORJSONModel):
+class NotificationBaseOUT(BaseSchemas):
     id: int
     title: str
     content: str
@@ -12,5 +15,3 @@ class NotificationBaseOUT(ORJSONModel):
     is_view: bool
     type: NotificationTypeEnum
     entity_id: int  | None = None
-    created_at: datetime
-    updated_at: datetime
