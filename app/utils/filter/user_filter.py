dfb01e0583ca89397301ccaf3f37f4842da386bc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-23 21:04:12 -0300

    Improvement in filter

--- app/utils/filter/user_filter.py
+++ app/utils/filter/user_filter.py
@@ -6,7 +6,8 @@ from app.configs.db.database import UserEntity
 
 class UserFilter(Filter):
     name__ilike: Optional[str] = Field(None, description="Search by name (case-insensitive).")
-    email__ilike: Optional[str] = Field(None, description="Search by name (case-insensitive).")
+    email__ilike: Optional[str] = Field(None, description="Search by email (case-insensitive).")
+    bio__ilike: Optional[str] = Field(None, description="Search by bio (case-insensitive).")
     created_at__gte: Optional[datetime] = Field(None, description="Filter by creation date, greater than or equal to.")
     created_at__lte: Optional[datetime] = Field(None, description="Filter by creation date, less than or equal to.")
 
